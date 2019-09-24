229e85a357cfa290e0f0f2cde1e7e6158f8fc937
Mattallmighty <matt@vibecommunications.co.nz>
2019-09-24 22:37:42 +1200

    frontend now showing options for WLED

--- ledfx/effects/wledeffect.py
+++ ledfx/effects/wledeffect.py
@@ -1,59 +1,34 @@
-from ledfx.wled import powerState
+from ledfx.wled import powerState, WLEDEffects, WLEDPalette, WLEDSpeed, WLEDIntensity, WLEDBrightness, WLEDHueColor
 from ledfx.effects.modulate import ModulateEffect
 from ledfx.effects.temporal import TemporalEffect
+from ledfx.effects.modulate import ModulateEffect
 import voluptuous as vol
 import numpy as np
 import urllib.request as url
 
-class WLEDEffect(TemporalEffect, ModulateEffect):
+class SingleColorEffect(TemporalEffect, ModulateEffect):
 
-    NAME = "powerState"
+    NAME = "Power State"
     CONFIG_SCHEMA = vol.Schema({
-        vol.Optional('wled', description='Color of strip', default = "aqua"): vol.In(list(powerState.keys())),
+        vol.Optional('Power LED', description='Power of LED', default = "Off"): vol.In(list(powerState.keys())),
+        vol.Optional('WLED Effects', description='WLED Effects', default = "Solid"): vol.In(list(WLEDEffects.keys())),
+        vol.Optional('Palette', description='Color Palette', default = "Solid"): vol.In(list(WLEDPalette.keys())),
+        vol.Optional('Speed', description='Effect Speed', default = "Med"): vol.In(list(WLEDSpeed.keys())),
+        vol.Optional('Effect Intensity', description='Effect Intensity', default = "Med"): vol.In(list(WLEDIntensity.keys())),
+        vol.Optional('Brightness', description='Brightness', default = "Bright - 80 Percent"): vol.In(list(WLEDBrightness.keys())),
+        vol.Optional('Primary Hue Color', description='Primary Hue Color', default = "Red"): vol.In(list(WLEDHueColor.keys())),
     })
 
     def config_updated(self, config):
-        self.wled = np.array(powerState[self._config['wled']], dtype=float)
+        self.wled = np.array(WLEDEffects[self._config['Power LED']], dtype=float)
 
     def effect_loop(self):
         wled_array = np.tile(self.wled, (self.pixel_count, 1))
         self.pixels = self.modulate(wled_array)
 
 
-
-
-#class WLEDColorEffect(TemporalEffect):
-#
-#    NAME = "Power"  
 #    CONFIG_SCHEMA = vol.Schema({
-#        vol.Optional('wled', description='WLED Power', default = "On"): vol.In(list(Power.keys())),
+#        vol.Optional('gradient_name', description='Preset gradient name', default = 'Spectral'): vol.In(list(GRADIENTS.keys())),
+#        vol.Optional('gradient_roll', description='Amount to shift the gradient', default = 0): vol.Coerce(int),
+#       vol.Optional('gradient_method', description='Function used to generate gradient', default = 'cubic_ease'): vol.In(["cubic_ease", "bezier"]),
 #    })
-#
-#    def config_updated(self, config):
-#        self.wled = np.array(Power[self._config['Power']], dtype=float)
-#
-#
-#    def effect_loop(self):
-#        color_array = np.tile(self.wled, (self.pixel_count, 1))
-#        self.pixels = self.modulate(color_array)
-# From **Singlecolor.py**
-#from ledfx.color import COLORS
-#from ledfx.effects.modulate import ModulateEffect
-#from ledfx.effects.temporal import TemporalEffect
-#from ledfx.effects.modulate import ModulateEffect
-#import voluptuous as vol
-#import numpy as np
-#
-#class SingleColorEffect(TemporalEffect, ModulateEffect):
-#
-#    NAME = "Single Color"
-#    CONFIG_SCHEMA = vol.Schema({
-#        vol.Optional('color', description='Color of strip', default = "red"): vol.In(list(COLORS.keys())),
-#    })
-#
-#    def config_updated(self, config):
-#        self.color = np.array(COLORS[self._config['color']], dtype=float)
-#
-#    def effect_loop(self):
-#        color_array = np.tile(self.color, (self.pixel_count, 1))
-#        self.pixels = self.modulate(color_array)
