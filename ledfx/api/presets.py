05225ba39d84c7e34a5c2f042ce25998d0a3f0e3
Matthew Bowley <m.bowley98@gmail.com>
2020-01-12 11:16:03 +0000

    PUT rename preset support
    PUT requests now must follow format
    {"action": "activate" or "rename",
    "id": preset_id,
    (if rename) "name": new_name}

--- ledfx/api/presets.py
+++ ledfx/api/presets.py
@@ -48,34 +48,51 @@ class PresetsEndpoint(RestEndpoint):
         """Activate a preset"""
         data = await request.json()
 
+        action = data.get('action')
+        if action is None:
+            response = { 'status' : 'failed', 'reason': 'Required attribute "action" was not provided' }
+            return web.Response(text=json.dumps(response), status=500)
+
+        if action not in ['activate', 'rename']:
+            response = { 'status' : 'failed', 'reason': 'Invalid action "{}"'.format(action) }
+            return web.Response(text=json.dumps(response), status=500)
+
         preset_id = data.get('id')
         if preset_id is None:
             response = { 'status' : 'failed', 'reason': 'Required attribute "preset_id" was not provided' }
             return web.Response(text=json.dumps(response), status=500)
 
         if not preset_id in self._ledfx.config['presets'].keys():
-            response = { 'status' : 'failed', 'reason': 'Preset {} does not exist'.format(preset_id) }
+            response = { 'status' : 'failed', 'reason': 'Preset "{}" does not exist'.format(preset_id) }
             return web.Response(text=json.dumps(response), status=500)
 
         preset = self._ledfx.config['presets'][preset_id]
 
-        for device in self._ledfx.devices.values():
-            # Check device is in preset, make no changes if it isn't
-            if not device.id in preset.keys():
-                _LOGGER.info(('Device with id {} has no data in preset {}').format(device.id, preset_id))
-                continue
+        if action == "activate":
+            for device in self._ledfx.devices.values():
+                # Check device is in preset, make no changes if it isn't
+                if not device.id in preset.keys():
+                    _LOGGER.info(('Device with id {} has no data in preset {}').format(device.id, preset_id))
+                    continue
 
-            # Set effect of device to that saved in the preset,
-            # clear active effect of device if no effect in preset
-            if preset[device.id]:
-                # Create the effect and add it to the device
-                effect = self._ledfx.effects.create(
-                    ledfx = self._ledfx,
-                    type = preset[device.id]['type'],
-                    config = preset[device.id]['config'])
-                device.set_effect(effect)
-            else:
-                device.clear_effect()
+                # Set effect of device to that saved in the preset,
+                # clear active effect of device if no effect in preset
+                if preset[device.id]:
+                    # Create the effect and add it to the device
+                    effect = self._ledfx.effects.create(
+                        ledfx = self._ledfx,
+                        type = preset[device.id]['type'],
+                        config = preset[device.id]['config'])
+                    device.set_effect(effect)
+                else:
+                    device.clear_effect()
+
+        elif action == "rename":
+            name = data.get('name')
+            if name is None:
+                response = { 'status' : 'failed', 'reason': 'Required attribute "name" was not provided' }
+                return web.Response(text=json.dumps(response), status=500)
+            self._ledfx.config['presets'][preset_id]['name'] = name
 
         response = { 'status' : 'success' }
         return web.Response(text=json.dumps(response), status=200)
