2e3ea3a864986092f309a1a891c92147e1f5a784
Austin Hodges <austin.b.hodges@gmail.com>
2020-01-14 14:23:23 -0800

    add effectlet list

---
+++ ledfx/effects/effectlets/__init__.py
@@ -0,0 +1,8 @@
+import os, fnmatch
+
+EFFECTLET_LIST = []
+
+files = os.listdir(os.path.dirname(__file__))
+for entry in files:
+    if fnmatch.fnmatch(entry, "*.npy"):
+        EFFECTLET_LIST.append(entry)
