f36fb4eae132cb18fa1db282a99b8849dab1029e
Austin Hodges <austin.b.hodges@gmail.com>
2020-01-15 22:24:15 -0800

    fix all colors

--- ledfx/color.py
+++ ledfx/color.py
@@ -18,29 +18,30 @@ COLORS = {
     'blue': RGB(0, 0, 255),
     'blue-light': RGB(65, 105, 225),
     'blue-navy': RGB(0, 0, 128),
+    'blue-aqua': RGB(0, 255, 255),
     'purple': RGB(128, 0, 128),
     'pink': RGB(255, 0, 178),
     'magenta': RGB(255, 0, 255),
     'black': RGB(0, 0, 0),
     'white': RGB(255, 255, 255),
-   'brown': RGB(139, 69, 19),
-   'gold': RGB(255, 215, 0),
-   'hotpink': RGB(255, 105, 180),
-   'lightblue': RGB(173, 216, 230),
-   'lightgreen': RGB(152, 251, 152),
-   'lightpink': RGB(255, 182, 193),
-   'lightyellow': RGB(255, 255, 224),
-   'maroon': RGB(128, 0, 0),
-   'mint': RGB(189, 252, 201),
-   'olive': RGB(85, 107, 47),
-   'peach': RGB(255, 100,100),
-   'plum': RGB(221, 160, 221),
-   'purple': RGB(128, 0, 128),
-   'sepia': RGB(94, 38, 18),
-   'skyblue': RGB(135, 206, 235),
-   'steelblue': RGB(70, 130, 180),
-   'tan': RGB(210, 180, 140),
-   'violetred': RGB(208, 32, 144),
+    'brown': RGB(139, 69, 19),
+    'gold': RGB(255, 215, 0),
+    'hotpink': RGB(255, 105, 180),
+    'lightblue': RGB(173, 216, 230),
+    'lightgreen': RGB(152, 251, 152),
+    'lightpink': RGB(255, 182, 193),
+    'lightyellow': RGB(255, 255, 224),
+    'maroon': RGB(128, 0, 0),
+    'mint': RGB(189, 252, 201),
+    'olive': RGB(85, 107, 47),
+    'peach': RGB(255, 100,100),
+    'plum': RGB(221, 160, 221),
+    'purple': RGB(128, 0, 128),
+    'sepia': RGB(94, 38, 18),
+    'skyblue': RGB(135, 206, 235),
+    'steelblue': RGB(70, 130, 180),
+    'tan': RGB(210, 180, 140),
+    'violetred': RGB(208, 32, 144),
 }
 
 GRADIENTS = {
@@ -60,13 +61,13 @@ GRADIENTS = {
         "colors": ["purple", "blue", "green-teal", "green", "yellow"]
     },
     "Jungle"    : {
-        "colors": ["green", "forestgreen", "orange"]
+        "colors": ["green", "green-forest", "orange"]
     },
     "Spring"     : {
         "colors": ["pink", "orange-deep", "yellow"]
     },
     "Winter"    : {
-        "colors": ["green-turquoise", "blue-green"]
+        "colors": ["green-turquoise", "green-coral"]
     },
     "Frost"    : {
         "colors": ["blue", "blue-aqua", "purple", "pink"]
