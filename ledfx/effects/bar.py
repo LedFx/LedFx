39b5f49dcf55ec9da910fd9d6cb4a90baa41f0f0
Matthew Bowley <m.bowley98@gmail.com>
2020-02-29 14:27:25 +0000

    Update bar.py
    code cleanup
    colour step option

--- ledfx/effects/bar.py
+++ ledfx/effects/bar.py
@@ -11,6 +11,7 @@ class BarAudioEffect(AudioReactiveEffect, GradientEffect):
         vol.Optional('gradient_name', description='Color scheme of bar', default = 'Spectral'): vol.In(list(GRADIENTS.keys())),
         vol.Optional('mode', description='Movement of bar', default = 'wipe'): vol.In(list(["bounce", "wipe", "in-out"])),
         vol.Optional('ease_method', description='Acceleration profile of bar', default='ease_out'): vol.In(list(["ease_in_out", "ease_in", "ease_out", "linear"])),
+        vol.Optional('color_step', description='Amount of color change per beat', default = 0.125): vol.All(vol.Coerce(float), vol.Range(min=0.0625, max=0.5))
     })
 
     def config_updated(self, config):
@@ -29,13 +30,15 @@ class BarAudioEffect(AudioReactiveEffect, GradientEffect):
         elif self._config["ease_method"] == "linear":
             x = beat_oscillator
 
+        # Colour change and phase
+        if beat_now:
+            self.phase = 1-self.phase # flip flop 0->1, 1->0
+            if self.phase == 0:
+                self.color_idx += self._config["color_step"] # 8 colours, 4 beats to a bar
+                self.color_idx = self.color_idx % 1 # loop back to zero
+
         # Compute position of bar start and stop
         if self._config["mode"] == "wipe":
-            if beat_now:
-                self.phase = 1-self.phase # flip flop 0->1, 1->0
-                if self.phase == 0:
-                    self.color_idx += 0.125 # 8 colours, 4 beats to a bar
-                    self.color_idx = self.color_idx % 1 # loop back to zero
             if self.phase == 0:
                 bar_end = x
                 bar_start = 0
@@ -44,11 +47,6 @@ class BarAudioEffect(AudioReactiveEffect, GradientEffect):
                 bar_start = x
 
         elif self._config["mode"] == "bounce":
-            bar_len = 0.3
-            if beat_now:
-                self.phase = 1-self.phase # flip flop 0->1, 1->0
-                self.color_idx += 0.125 # 8 colours, 4 beats to a bar
-                self.color_idx = self.color_idx % 1 # loop back to zero
             x = x*(1-bar_len)
             if self.phase == 0:
                 bar_end = x+bar_len
@@ -58,11 +56,6 @@ class BarAudioEffect(AudioReactiveEffect, GradientEffect):
                 bar_start = 1-(x+bar_len)
 
         elif self._config["mode"] == "in-out":
-            if beat_now:
-                self.phase = 1-self.phase # flip flop 0->1, 1->0
-                if self.phase == 0:
-                    self.color_idx += 0.125 # 8 colours, 4 beats to a bar
-                    self.color_idx = self.color_idx % 1 # loop back to zero
             if self.phase == 0:
                 bar_end = x
                 bar_start = 0
