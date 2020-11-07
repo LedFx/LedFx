b64d71309d292f39b02a0327da84a8897f77ee78
Matthew Bowley <m.bowley98@gmail.com>
2020-11-07 16:33:36 +0000

    update consts to 0.8.3

--- ledfx/consts.py
+++ ledfx/consts.py
@@ -4,13 +4,13 @@ __author__ = "Austin Hodges"
 __copyright__ = "Austin Hodges"
 __license__ = "mit"
 
-REQUIRED_PYTHON_VERSION = (3, 6, 0)
+REQUIRED_PYTHON_VERSION = (3, 7, 0)
 REQUIRED_PYTHON_STRING = '>={}.{}.{}'.format(REQUIRED_PYTHON_VERSION[0],
                                              REQUIRED_PYTHON_VERSION[1],
                                              REQUIRED_PYTHON_VERSION[2])
 
 MAJOR_VERSION = 0
-MINOR_VERSION = 1
+MINOR_VERSION = 7
 MICRO_VERSION = 1
 DEV_VERSION   = 2
 PROJECT_VERSION = '{}.{}.{}'.format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
