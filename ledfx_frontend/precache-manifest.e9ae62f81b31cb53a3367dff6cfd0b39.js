16e9dc3e4238d3ee00853aabe11259e7eb65fb92
thatdonfc <thatdonfc+git@gmail.com>
2020-11-09 14:05:00 -0800

    Revert "Revert package.json and build frontend, bump dev3"
    This reverts commit aa56e5d17fdd8480663974b19bb2fea3ace24ef9.

---
+++ ledfx_frontend/precache-manifest.e9ae62f81b31cb53a3367dff6cfd0b39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ff2c649a6fc16c21df530fdbd51af1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c55eea486e055ba6d213",
+    "url": "/static/css/main.5f265409.chunk.css"
+  },
+  {
+    "revision": "698faaffab497ea26699",
+    "url": "/static/js/2.4f829b51.chunk.js"
+  },
+  {
+    "revision": "d9102df1a3cc2f72cfa727491b26d8fd",
+    "url": "/static/js/2.4f829b51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c55eea486e055ba6d213",
+    "url": "/static/js/main.8b9fb34a.chunk.js"
+  },
+  {
+    "revision": "09b2009ed18140fb7100",
+    "url": "/static/js/runtime-main.5d286450.js"
+  }
+]);
