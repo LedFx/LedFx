4a022c8dbc1029a9c7571265c53b3ea10f2f4c67
Shaun Eccles-Smith <shauneccles@gmail.com>
2020-12-05 10:15:33 +1100

    Installer Fix

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1090
+sudo apt-get install python3-pip
 cd ~
 mkdir ledfx-workdir
 cd ledfx-workdir
-git pull -b dev https://github.com/ahodges9/LedFx/
+git clone -b dev https://github.com/ahodges9/LedFx/
 cd LedFx
 pip install -e .
 python setup.py build
