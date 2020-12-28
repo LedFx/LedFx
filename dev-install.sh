4c720cc0a14e59138ca9714da7d30276a19d7ff8
Shaun Eccles-Smith <shauneccles@gmail.com>
2020-12-28 20:29:01 +1100

    Move to pip install from git, interactive install

--- dev-install.sh
+++ dev-install.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 # shellcheck disable=SC1090
 echo "  _                   _   ______                          ";
@@ -25,42 +24,62 @@ sudo apt-get install -y gcc \
         python3-pip
 python3 -m pip install  --upgrade pip wheel setuptools
 echo "Removing prior ledfx installation data"
-python3 -m pip uninstall -y ledfx
-python3 -m pip uninstall -y ledfx-dev
+python3 -m pip -q uninstall -y ledfx
+python3 -m pip -q uninstall -y ledfx-dev
 sudo systemctl stop ledfx
 sudo systemctl disable ledfx
 sudo rm /etc/systemd/system/ledfx.service
 curruser=$USER
 IP=$(/sbin/ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')
 rm -rf ~/ledfx-workdir
-echo "Downloading latest version from github"
-git clone --depth 1 -b dev https://github.com/LedFx/LedFx ~/ledfx-workdir
-python3 -m pip install --user -r ~/ledfx-workdir/requirements.txt
-python3 ~/ledfx-workdir/setup.py build
-python3 ~/ledfx-workdir/setup.py install --user
+echo "Downloading and installing latest version from github"
+python3 -m pip install git+https://github.com/LedFx/LedFx@dev
 echo "Adding" $curruser "to Audio Group"
 sudo usermod -a -G audio $curruser
-echo "Updating alsa.conf - backup created in /usr/share/alsa/alsa.conf.bak"
-sudo sed --in-place=.bak 's/defaults.ctl.card 0/defaults.ctl.card 1/' /usr/share/alsa/alsa.conf
-sudo sed -i 's/defaults.pcm.card 0/defaults.pcm.card 1/' /usr/share/alsa/alsa.conf
+
+read -p "Are you using a USB sound device for input? (y/n) " RESP
+if [ "$RESP" = "y" ]; then
+  echo "Updating alsa.conf - backup created in /usr/share/alsa/alsa.conf.bak"
+  sudo sed --in-place=.bak 's/defaults.ctl.card 0/defaults.ctl.card 1/' /usr/share/alsa/alsa.conf
+  sudo sed -i 's/defaults.pcm.card 0/defaults.pcm.card 1/' /usr/share/alsa/alsa.conf
+fi
+
+read -p "Create Audio loopback for sound playing from this device? (y/n) " RESP
+if [ "$RESP" = "y" ]; then
+
+  sudo modprobe snd-aloop
+  echo "Adding alsa loopback to /etc/modules for next boot"
+  echo "snd-aloop" | sudo tee -a /etc/modules
+
+fi
+
+read -p "Install as Service? (y/n) " RESP
+if [ "$RESP" = "y" ]; then
+
 echo "Creating Service"
 echo "[Unit]
 Description=LedFx Music Visualizer
-After=network.target
-Wants=network-online.target
+After=network.target sound.target
 StartLimitIntervalSec=0
+
 [Service]
 Type=simple
 Restart=always
-RestartSec=1
+RestartSec=5
 User="$curruser"
+Group=audio
 ExecStart=/usr/bin/python3 /home/"$curruser"/.local/bin/ledfx
 [Install]
 WantedBy=multi-user.target
-" >> ~/ledfx-workdir/ledfx.service
-sudo cp ~/ledfx-workdir/ledfx.service /etc/systemd/system/ledfx.service
+" >> ~/ledfx.service
+sudo mv ~/ledfx.service /etc/systemd/system/ledfx.service
 sudo systemctl enable ledfx
 sudo systemctl start ledfx
 sudo systemctl status ledfx
 echo "LedFx is now running. Please navigate to "$IP":8888 in your web browser"
 echo "You may need to restart your pi for the sound device to be accessable"
+
+else
+
+echo "LedFx is now installed. Please type ledfx to start."
+fi
