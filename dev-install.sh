20fd975a97e615ebf2f742d1248519b958f996a2
shauneccles <shauneccles@gmail.com>
2020-12-29 08:42:28 +1100

    Move to whiptail prompts
    Turns out you can't pipe read, so it worked on local test files but not via curl. This works.

--- dev-install.sh
+++ dev-install.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1090
+if [ -t 0 ] ; then
+  screen_size=$(stty size)
+else
+  screen_size="24 80"
+fi
+# Set rows variable to contain first number
+printf -v rows '%d' "${screen_size%% *}"
+# Set columns variable to contain second number
+printf -v columns '%d' "${screen_size##* }"
+
+# Divide by two so the dialogs take up half of the screen, which looks nice.
+r=$(( rows / 2 ))
+c=$(( columns / 2 ))
+# Unless the screen is tiny
+r=$(( r < 20 ? 20 : r ))
+c=$(( c < 70 ? 70 : c ))
 echo "  _                   _   ______                          ";
 echo " | |                 | | |  ____|                         ";
 echo " | |        ___    __| | | |__    __  __                  ";
@@ -13,7 +29,7 @@ echo "   | |   | '_ \  / __| | __|  / _\` | | | | |  / _ \ | '__|";
 echo "  _| |_  | | | | \__ \ | |_  | (_| | | | | | |  __/ | |   ";
 echo " |_____| |_| |_| |___/  \__|  \__,_| |_| |_|  \___| |_|   ";
 echo "                                                          ";
-echo "Installing LedFx requirements"
+whiptail --msgbox --backtitle "Welcome to LedFx Installer" --title "LedFx automated installer" "\\n\\nThis installer will transform your device into a LedFx powered LED driver for networked LEDs! \\n\\n\\n\\nPlease visit ledfx.app or the LedFx Discord if you have any issues" "${r}" "${c}"
 sudo apt-get update
 sudo apt-get install -y gcc \
         git \
@@ -36,25 +52,26 @@ echo "Downloading and installing latest version from github"
 python3 -m pip install git+https://github.com/LedFx/LedFx@dev
 echo "Adding" $curruser "to Audio Group"
 sudo usermod -a -G audio $curruser
-
-read -p "Are you using a USB sound device for input? (y/n) " RESP
-if [ "$RESP" = "y" ]; then
+whiptail --yesno "Are you using a USB sound device for input?" --yes-button "Yes" --no-button "No" "${r}" "${c}"
+USB=$?
+if [ "$USB" = "0" ]; then
   echo "Updating alsa.conf - backup created in /usr/share/alsa/alsa.conf.bak"
   sudo sed --in-place=.bak 's/defaults.ctl.card 0/defaults.ctl.card 1/' /usr/share/alsa/alsa.conf
   sudo sed -i 's/defaults.pcm.card 0/defaults.pcm.card 1/' /usr/share/alsa/alsa.conf
 fi
+whiptail --yesno "Create Audio loopback for sound playing from this device?" --yes-button "Yes" --no-button "No" "${r}" "${c}" 
+LOOPBACK=$?
 
-read -p "Create Audio loopback for sound playing from this device? (y/n) " RESP
-if [ "$RESP" = "y" ]; then
+if [ "$LOOPBACK" = "0" ]; then
 
   sudo modprobe snd-aloop
   echo "Adding alsa loopback to /etc/modules for next boot"
   echo "snd-aloop" | sudo tee -a /etc/modules
 
 fi
-
-read -p "Install as Service? (y/n) " RESP
-if [ "$RESP" = "y" ]; then
+whiptail --yesno "Create service so LedFx launches automatically on boot?" --yes-button "Yes" --no-button "No" "${r}" "${c}"
+SERVICE=$?
+if [ "$SERVICE" = "0" ]; then
 
 echo "Creating Service"
 echo "[Unit]
