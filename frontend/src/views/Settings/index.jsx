72ce528a4f218b4a170e89125de148da6e0123f1
YeonV <trusted.yeon@gmail.com>
2021-01-12 19:36:18 +0100

    cleanup

--- frontend/src/views/Settings/index.jsx
+++ frontend/src/views/Settings/index.jsx
@@ -2,19 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import Button from '@material-ui/core/Button';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
-import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 
 import { getAudioInputs, setAudioInput } from 'modules/settings';
 
 import AudioInputCard from './AudioInput';
 import ConfigCard from './ConfigCard';
-import Divider from '@material-ui/core/Divider';
 
 const styles = theme => ({});
 
