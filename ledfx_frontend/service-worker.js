9d6177abecae4baba72eb1cb45e7326371e5af37
YeonV <trusted.yeon@gmail.com>
2021-01-16 19:55:15 +0000

    try new build

--- ledfx_frontend/service-worker.js
+++ ledfx_frontend/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e812f73675b1349541482d98f1866c48.js"
+  "/precache-manifest.7bc9663ddf4bfd1119b45f371f88a2fc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -34,6 +34,6 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-
+  
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
