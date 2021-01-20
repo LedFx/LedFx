68586ba190ffc28ee585769008b5350a11ecb12c
YeonV <trusted.yeon@gmail.com>
2021-01-20 18:44:54 +0000

    New Build

---
+++ ledfx_frontend/precache-manifest.2c5100ed7aab7257042605a6e716861b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200f93a2a735710ced892cd686d9d754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42403575d203a2ac6316",
+    "url": "/static/css/main.5f265409.chunk.css"
+  },
+  {
+    "revision": "6815431bd6b04ea45362",
+    "url": "/static/js/2.cea2e220.chunk.js"
+  },
+  {
+    "revision": "7f7a0ea95c540f1bb224d175b84fb41a",
+    "url": "/static/js/2.cea2e220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42403575d203a2ac6316",
+    "url": "/static/js/main.d37c9e1e.chunk.js"
+  },
+  {
+    "revision": "09b2009ed18140fb7100",
+    "url": "/static/js/runtime-main.5d286450.js"
+  }
+]);
