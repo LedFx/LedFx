a44f86ec9cd7744ca46ca2a792b001e5b65ca0b8
YeonV <trusted.yeon@gmail.com>
2021-01-28 16:14:02 +0000

    finish convert to functional component for "DisplayView"

--- frontend/src/views/Display/index.jsx
+++ frontend/src/views/Display/index.jsx
@@ -1,65 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
 
 import { loadDisplayInfo, setDisplayEffect, clearDisplayEffect } from 'modules/selectedDisplay';
 import { activatePreset, getEffectPresets, addPreset } from 'modules/presets';
 import DisplayEffectControl from 'components/EffectControl/DisplayEffectControl';
-// import EffectControlBlade from 'components/EffectControl/blade';
 import DisplayPixelColorGraph from 'components/PixelColorGraph/DisplayPixelColorGraph';
 import PresetsCard from 'components/PresetsCard';
 import { fetchDisplayList } from 'modules/displays';
+import MoreInfo from './MoreInfo';
 
 const DisplayView = ({
     match: {
         params: { displayId },
     },
-    presets,
-    schemas,
-    selectedDisplay,
-    addPreset,
-    activatePreset,
-    clearDisplayEffect,
-    loadDisplayInfo,
-    setDisplayEffect,
-    getEffectPresets,
 }) => {
-    const [state, setState] = useState();
-
-    useEffect(() => {
-        if (displayId !== state) {
-            loadDisplayInfo(displayId);
-            setState(displayId);
-            fetchDisplayList();
-        }
-    }, [displayId, loadDisplayInfo, state]);
-    useEffect(() => {
-        fetchDisplayList();
-        setState(displayId);
-    }, [displayId]);
-
-    // const handleLoadDisplay = displayId => {
-    //     loadDisplayInfo(displayId);
-    // };
-
-    const handleClearEffect = displayId => {
-        clearDisplayEffect(displayId);
-    };
-
-    const handleSetEffect = data => {
-        setDisplayEffect(data.displayId, data);
-    };
-
-    const handleTypeChange = effectType => {
-        getEffectPresets(effectType);
-    };
-
+    const schemas = useSelector(state => state.schemas);
+    const selectedDisplay = useSelector(state => state.selectedDisplay);
     const { display, effect, isDisplayLoading } = selectedDisplay;
+    const presets = useSelector(state => state.presets);
+    const dispatch = useDispatch();
+    const [dispId, setDispId] = useState();
+
+    useEffect(() => {
+        if (displayId !== dispId) {
+            dispatch(loadDisplayInfo(displayId));
+            setDispId(displayId);
+        }
+    }, [displayId, dispId, dispatch]);
+
+    useEffect(() => {
+        dispatch(fetchDisplayList());
+    }, [dispatch]);
 
     if (schemas.isLoading || isDisplayLoading || !display) {
         return <p>Loading...</p>;
@@ -87,9 +62,9 @@ const DisplayView = ({
                                 display={display}
                                 effect={effect}
                                 schemas={schemas}
-                                onClear={handleClearEffect}
-                                onSubmit={handleSetEffect}
-                                onTypeChange={handleTypeChange}
+                                onClear={() => dispatch(clearDisplayEffect(displayId))}
+                                onSubmit={data => dispatch(setDisplayEffect(data.displayId, data))}
+                                onTypeChange={effectType => dispatch(getEffectPresets(effectType))}
                             />
                         </CardContent>
                     </Card>
@@ -100,72 +75,15 @@ const DisplayView = ({
                             device={display}
                             presets={presets}
                             effect={effect}
-                            activatePreset={activatePreset}
-                            getEffectPresets={getEffectPresets}
-                            addPreset={addPreset}
+                            activatePreset={() => dispatch(activatePreset)}
+                            getEffectPresets={() => dispatch(getEffectPresets)}
+                            addPreset={() => dispatch(addPreset)}
                         />
                     )}
                 </Grid>
-                {/* {parseInt(window.localStorage.getItem('BladeMod')) > 2 && (
-                    <Grid item xs={12} lg={6}>
-                        <Card>
-                            <CardContent>
-                                <EffectControlBlade
-                                    display={display}
-                                    effect={effect}
-                                    schemas={schemas}
-                                    onClear={handleClearEffect}
-                                    onSubmit={handleSetEffect}
-                                    onTypeChange={handleTypeChange}
-                                />
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                )} */}
+
                 {parseInt(window.localStorage.getItem('BladeMod')) > 1 && (
-                    <>
-                        <Grid item xs={6} lg={6}>
-                            <Card>
-                                <CardContent>
-                                    <Typography variant="h5">Display Config</Typography>
-                                    <Typography variant="subtitle1">
-                                        Total Pixels: {display.config[display.id].pixel_count}
-                                    </Typography>
-                                    <br />
-                                    <Typography variant="caption">
-                                        Active: {JSON.stringify(display.config[display.id].active)}
-                                        <br />
-                                        Center Offset:{' '}
-                                        {display.config[display.id].config.center_offset}
-                                        <br />
-                                        Crossfade: {display.config[display.id].config.crossfade}
-                                        <br />
-                                        Max Brightness:{' '}
-                                        {display.config[display.id].config.crossfade}
-                                        <br />
-                                        Preview only:{' '}
-                                        {JSON.stringify(
-                                            display.config[display.id].config.preview_only
-                                        )}
-                                    </Typography>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                        <Grid item xs={6} lg={6}>
-                            <Card>
-                                <CardContent>
-                                    <Typography variant="h5">Display Segments</Typography>
-                                    <Typography variant="subtitle1">
-                                        Segments: {display.config[display.id].segments.length}
-                                    </Typography>
-                                    <br />
-                                    {display.config[display.id].segments.map((s, i) => (
-                                        <li key={i}>{s.join(',')}</li>
-                                    ))}
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    </>
+                    <MoreInfo display={display} />
                 )}
             </Grid>
         </>
@@ -186,25 +104,4 @@ const renderPixelGraph = (display, effect) => {
     }
 };
 
-DisplayView.propTypes = {
-    schemas: PropTypes.object.isRequired,
-    selectedDisplay: PropTypes.object.isRequired,
-};
-
-export default connect(
-    state => ({
-        schemas: state.schemas,
-        selectedDisplay: state.selectedDisplay,
-        clearDisplayEffect: state.clearDisplayEffect,
-        presets: state.presets,
-    }),
-    {
-        loadDisplayInfo,
-        clearDisplayEffect,
-        setDisplayEffect,
-        activatePreset,
-        getEffectPresets,
-        addPreset,
-        fetchDisplayList,
-    }
-)(DisplayView);
+export default DisplayView;
