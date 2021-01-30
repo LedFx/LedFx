e591105c387bf44bb1eb6929c7ba5a5edb316c1a
Shaun Eccles-Smith <shauneccles@gmail.com>
2021-01-31 10:23:58 +1100

    Update comments for Sentry_config

--- ledfx/sentry_config.py
+++ ledfx/sentry_config.py
@@ -3,9 +3,9 @@ from sentry_sdk.integrations.aiohttp import AioHttpIntegration
 
 from ledfx.consts import PROJECT_VERSION
 
-# This key is overwritten with our organizational sentry key as part of the deployment process.
-# You're welcome to plug your own sentry key in if you're forking/doing development - sentry.io
-# This current key is disabled and no data will be sent to anyone if you use this sentry key.
+# Place your key between the quotes if you have a sentry.io account and wish to use it.
+# Otherwise the LedFx sentry key is inserted here during deployment.
+
 
 sentry_dsn = ""
 
