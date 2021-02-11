762efc1577b20028c3f0f9f4d0f41bdf5f1d32dc
YeonV <trusted.yeon@gmail.com>
2021-02-11 00:21:27 +0000

    Fix timeout

--- ledfx/devices/wled.py
+++ ledfx/devices/wled.py
@@ -17,7 +17,7 @@ class WLEDDevice(DDPDevice):
                 "timeout",
                 description="Time between LedFx effect off and WLED effect activate",
                 default=2.0,
-            ): float,
+            ): vol.All(vol.Coerce(float), vol.Range(0, 10)),
         }
     )
 
