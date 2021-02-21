963e1942a1f79f40ea312c3697400e295bf3e95a
YeonV <trusted.yeon@gmail.com>
2021-02-21 22:24:35 +0100

    playground for not_matt

--- frontend/src/components/TransitionCard/index.jsx
+++ frontend/src/components/TransitionCard/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -11,6 +11,11 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import SaveIcon from '@material-ui/icons/Save';
 import { addDisplay } from 'modules/displays'
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import Slider from "@material-ui/core/Slider";
 
 // CONSTANT display categories
 export const DEFAULT_CAT = 'default_presets';
@@ -35,17 +40,41 @@ const TransitionCard = ({ display, config, addDisplay }) => {
     const classes = useStyles();
     const dispatch = useDispatch();
 
-    const handleSetTransition = (displayId, config) => () => {
-        dispatch(addDisplay({"id": displayId, "config": config}));
-    };
+    const schemas = useSelector(state => state.schemas.displays.schema.properties)
 
+    const transition_mode = display.config[display.id] && display.config[display.id].config && display.config[display.id].config.transition_mode
+    const transition_time = display.config[display.id] && display.config[display.id].config && display.config[display.id].config.transition_time
+    console.log("YZ-NOTMATT1:", transition_mode, transition_time)
+    console.log("YZ-NOTMATT2:", schemas.transition_mode.enum, schemas.transition_time)
+    const handleSetTransition = (displayId, config) => () => {
+        dispatch(addDisplay({ "id": displayId, "config": config }));
+    };
+    const addDevice = () => {
+        console.log('adding now')
+    }
     return (
         <Card variant="outlined">
             <CardHeader title="Transitions" subheader="Seamlessly blend between effects" />
             <CardContent className={classes.content}>
-                <Typography variant="body1" color="textSecondary">
-                    Save this effect configuration as a preset
-                </Typography>
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="demo-simple-select-helper-label">Save this effect configuration as a preset</InputLabel>
+                    <Select onChange={addDevice} >
+                        {schemas.transition_mode.enum.map(mode => <MenuItem value={mode}>{mode}</MenuItem>)}
+                    </Select>
+                </FormControl>
+                <Slider
+                    defaultValue={schemas.transition_time.default}
+                    // value={transitionTime}
+                    // onChange={(event, value) => setTransitionTime(value)}
+                    aria-labelledby="discrete-slider-always"
+                    step={0.1}
+                    // marks={marks}
+                    min={schemas.transition_time.minimum}
+                    max={schemas.transition_time.maximum}
+                    // disabled={type === "instant"}
+                    valueLabelDisplay="on"
+                />
+
             </CardContent>
         </Card>
     );
