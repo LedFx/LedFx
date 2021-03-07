fd01d7d5872d63976bf8c8b727eeceb37384c43b
thatdonfc <thatdonfc+git@gmail.com>
2021-03-07 08:19:15 -0800

    Bump sentry_sdk, setuptools

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ INSTALL_REQUIRES = [
     'pypiwin32>=223; platform_system == "Windows"',
     "cython>=0.29.21",
     "pyupdater>=3.1.0",
-    "sentry-sdk>=0.19.0",
+    "sentry-sdk==1.0.0",
     "certifi>=2019.3.9",
     "pyserial>=3.5",
 ]
