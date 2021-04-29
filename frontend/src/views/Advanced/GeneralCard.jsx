3aec2f017a3fb9023098970c6849d81e5f5c3c59
YeonV <trusted.yeon@gmail.com>
2021-04-29 03:38:17 +0200

    fix export config json

--- frontend/src/views/Advanced/GeneralCard.jsx
+++ frontend/src/views/Advanced/GeneralCard.jsx
@@ -32,7 +32,7 @@ const GeneralCard = () => {
     const { audioInputs } = settings;
     function download(content, fileName, contentType) {
         var a = document.createElement("a");
-        var file = new Blob([content], { type: contentType });
+        var file = new Blob([JSON.stringify(content, null, 4)], { type: contentType });
         a.href = URL.createObjectURL(file);
         a.download = fileName;
         a.click();
