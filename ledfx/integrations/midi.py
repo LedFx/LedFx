3c40747a0862444749a380b0087efb0af1f09da2
Matthew Bowley <m.bowley98@gmail.com>
2021-05-31 22:42:46 +0100

    Midi mapper queue wip

--- ledfx/integrations/midi.py
+++ ledfx/integrations/midi.py
@@ -194,6 +194,8 @@ class MIDI(Integration):
             # do something when mapping doesn't work
 
         self.mapping = Mapping(mapping_dict)
+        self.message_queue = set()
+        self.hold_queue_flag = False
 
     def restore_from_data(self, data):
         """ Might be used in future """
@@ -248,13 +250,67 @@ class MIDI(Integration):
                 del self._data[scene_id][trigger_id]
 
     def handle_message(self, message):
+        message = frozen.freeze_message(message)
         try:
             region = next(
                 region for region in self.mapping.regions if message in region
             )
             _LOGGER.info(f"Received input to region: {region}: {message}")
         except StopIteration:
-            _LOGGER.info(f"Received input to unmapped region: {message}")
+            return
+            # _LOGGER.info(f"Received input to unmapped region: {message}")
+
+        # special case, handle holding the queue
+        # if region.function == "queue hold": or whatever, future me will figure that one out
+        if message == mido.Message(
+            "note_on", channel=0, note=98, velocity=127
+        ):
+            self.hold_queue_flag = not self.hold_queue_flag
+            print(f"QUEUE {'PAUSED' if self.hold_queue_flag else 'UNPAUSED'}")
+            if not self.hold_queue_flag:
+                self.process_message_queue()
+            return
+
+        if region.input_type == 0:
+            # if button press not in queue, add it
+            # else if button press in queue, remove it (ie cancel the action if queue is held)
+            try:
+                self.message_queue.remove(message)
+            except KeyError:
+                self.message_queue.add(message)
+        elif region.input_type == 1:
+            # if message with same type, position, const in queue, remove it
+            # before adding the new one.
+            # this ensures that if the queue is held, then a slider moved, when
+            # the queue is released only the most recent slider value is processed
+            pos = region.midi_input["POSITION_VALUE"]
+            const = region.midi_input["CONST_VALUE"]
+            for q_message in self.message_queue:
+                if (
+                    q_message.type == message.type
+                    and getattr(q_message, pos) == getattr(message, pos)
+                    and getattr(q_message, const) == getattr(message, const)
+                ):
+                    # removing item while iterating? 'ware the moon...
+                    self.message_queue.remove(q_message)
+                    break
+            self.message_queue.add(message)
+        elif region.input_type == 2:
+            # who knows what this even means
+            # wheels are pretty undefined rn
+            self.message_queue.add(message)
+
+        self.process_message_queue()
+
+    def process_message_queue(self):
+        """
+        this is the big cheese function, all the action.
+        handles stuff. what stuff? to be continued...
+        """
+        if not self.hold_queue_flag:
+            for _ in range(len(self.message_queue)):
+                message = self.message_queue.pop()
+                print(f"handling message: {message}")
 
     async def connect(self):
         midi_device = self._config["midi_device"]
@@ -383,16 +439,6 @@ class Region:
             )
         }
 
-    def as_dict(self):
-        return {
-            "NAME": self.name,
-            "DIMENSIONALITY": self.dimensionality,
-            "INPUT_TYPE": self.input_type,
-            "MIDI_INPUT": self.midi_input,
-            "LED_COLOUR_RANGE": self.led_colour_range,
-            "LED_STATE_MAPPINGS": self.led_state_mappings,
-        }
-
     def __repr__(self):
         return f"'{self.name}'"
 
@@ -410,7 +456,7 @@ class Region:
         Check if a message falls within this region's
         scope
         """
-        if msg.type == self.midi_input["MSG_TYPE"]:
-            if not frozen.is_frozen(msg):
-                msg = frozen.freeze_message(msg)
-            return msg.copy(**self._default_motion) in self._input_positions
+        if msg.type != self.midi_input["MSG_TYPE"]:
+            return False
+
+        return msg.copy(**self._default_motion) in self._input_positions
