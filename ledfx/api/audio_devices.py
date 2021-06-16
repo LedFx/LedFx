5814b7aa9a7962800dd1427a8f1373297b8bcd35
Matthew Bowley <m.bowley98@gmail.com>
2021-06-16 18:12:20 +0100

    Cleanup audio device api

--- ledfx/api/audio_devices.py
+++ ledfx/api/audio_devices.py
@@ -21,30 +21,23 @@ class AudioDevicesEndpoint(RestEndpoint):
     async def get(self) -> web.Response:
         """Get list of audio devices using sound device"""
         # Need to map out what host API actually means - however for now, just display it
-        if self._audio is None:
-            self._audio = sd
-        hostapis = self._audio.query_hostapis()
 
-        devices = self._audio.query_devices()
-        input_devices = []
-        for device in devices:
-            if device["max_input_channels"] > 0:
-                device["index"] = devices.index(device)
-                input_devices += [device]
+        hostapis = sd.query_hostapis()
+        devices = sd.query_devices()
+
+        input_devices = list(
+            f"{hostapis[device['hostapi']]['name']}: {device['name']}"
+            for idx, device in enumerate(devices)
+            if device["max_input_channels"] > 0
+        )
 
         audio_config = self._ledfx.config.get("audio", {"device_index": 0})
-        input_device_count = len(input_devices)
 
-        audio_devices = {}
-        audio_devices["devices"] = {}
-        audio_devices["active_device_index"] = audio_config["device_index"]
-        for i in range(0, input_device_count):
-            audio_devices["devices"][i] = (
-                input_devices[i]["name"]
-                + " using host API : "
-                + str(input_devices[i]["hostapi"])
-            )
-        return web.json_response(data=audio_devices, status=200)
+        response = {}
+        response["active_device_index"] = audio_config["device_index"]
+        response["devices"] = dict(enumerate(input_devices))
+
+        return web.json_response(data=response, status=200)
 
     async def put(self, request) -> web.Response:
         """Set audio device to use as input"""
@@ -56,15 +49,8 @@ class AudioDevicesEndpoint(RestEndpoint):
                 "reason": "JSON Decoding failed",
             }
             return web.json_response(data=response, status=400)
+
         index = data.get("index")
-
-        devices = self._audio.query_devices()
-        input_devices = []
-        for device in devices:
-            if device["max_input_channels"] > 0:
-                device["index"] = devices.index(device)
-                input_devices += [device]
-
         if index is None:
             response = {
                 "status": "failed",
@@ -72,7 +58,14 @@ class AudioDevicesEndpoint(RestEndpoint):
             }
             return web.json_response(data=response, status=400)
 
-        if index not in range(0, len(input_devices)):
+        valid_indexes = range(
+            sum(
+                device["max_input_channels"] > 0
+                for device in sd.query_devices()
+            )
+        )
+
+        if index not in valid_indexes:
             response = {
                 "status": "failed",
                 "reason": f"Invalid device index [{index}]",
