2f7e4ec18718f032cbda686944cca86ec8302122
Matthew Bowley <m.bowley98@gmail.com>
2021-06-21 22:00:06 +0100

    Revert default port to 8888

--- ledfx/config.py
+++ ledfx/config.py
@@ -65,7 +65,7 @@ WLED_CONFIG_SCHEMA = vol.Schema(
 CORE_CONFIG_SCHEMA = vol.Schema(
     {
         vol.Optional("host", default="0.0.0.0"): str,
-        vol.Optional("port", default=8080): int,
+        vol.Optional("port", default=8888): int,
         vol.Optional("port_s", default=8443): int,
         vol.Optional("dev_mode", default=False): bool,
         vol.Optional("devices", default=[]): list,
