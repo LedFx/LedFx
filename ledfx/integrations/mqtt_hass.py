4d0fd7e41fccfb83714eb3e62b0303043ab31e92
Yeon Vinzenz Varapragasam <trusted.yeon@gmail.com>
2021-07-26 12:58:41 +0200

    mqtt-hass use new SELECT component

--- ledfx/integrations/mqtt_hass.py
+++ ledfx/integrations/mqtt_hass.py
@@ -68,17 +68,18 @@ class MQTT_HASS(Integration):
         self._listeners = []
 
     def on_connect(self, client, userdata, flags, rc):
-        ### Create Scene-Selector and Transition-Config as Light in HomeAssistant
-        client.subscribe(f"{self._config['topic']}/light/ledfxscene/set")
+        ### Create Scene-Selector and Transition-Config as Light in HomeAssistant      
+        
+
+        # SCENE SELECTOR
         client.subscribe(f"{self._config['topic']}/select/ledfxsceneselect/set")
-        client.subscribe(f"{self._config['topic']}/light/ledfxtransition/set")
         client.publish(f"{self._config['topic']}/select/ledfxsceneselect/config", json.dumps({
             "~": f"{self._config['topic']}/select/ledfxsceneselect",
-            "name": "LedFx Scene Selector",
+            "name": "1 Scene Selector",
             "unique_id": "ledfxsceneselect",
             "cmd_t": "~/set",
             "stat_t": "~/state",
-            "icon":  "mdi:image-outline",
+            "icon":  "mdi:image-multiple-outline",
             "options": list(self._ledfx.config["scenes"].keys()),
             "device": {
                 "identifiers": [
@@ -90,40 +91,17 @@ class MQTT_HASS(Integration):
                 "sw_version": self._ledfx.config['configuration_version']
             }
         }))
-        client.publish(f"{self._config['topic']}/light/ledfxscene/config", json.dumps({
-            "~": f"{self._config['topic']}/light/ledfxscene",
-            "name": "LedFx Scene",
-            "unique_id": "ledfxscene",
+
+        # TRANSITION TYPE
+        client.subscribe(f"{self._config['topic']}/select/ledfxtransitiontype/set")
+        client.publish(f"{self._config['topic']}/select/ledfxtransitiontype/config", json.dumps({
+            "~": f"{self._config['topic']}/select/ledfxtransitiontype",
+            "name": "2 Transition Type",
+            "unique_id": "ledfxtransitiontype",
             "cmd_t": "~/set",
             "stat_t": "~/state",
-            "schema": "json",
-            "brightness": False,
-            "brightness_scale": 1000,
-            "icon":  "mdi:image-outline",
-            "effect": True,
-            "effect_list": list(self._ledfx.config["scenes"].keys()),
-            "device": {
-                "identifiers": [
-                    "yzlights"
-                ],
-                "name": "LedFx",
-                "model": "BladeMOD",
-                "manufacturer": "Yeon",
-                "sw_version": self._ledfx.config['configuration_version']
-            }
-        }))
-        client.publish(f"{self._config['topic']}/light/ledfxtransition/config", json.dumps({
-            "~": f"{self._config['topic']}/light/ledfxtransition",
-            "name": "LedFx Transition",
-            "unique_id": "ledfxtransition",
-            "cmd_t": "~/set",
-            "stat_t": "~/state",
-            "schema": "json",
-            "brightness": True,
-            "brightness_scale": 1000,
-            "icon":  "mdi:image-outline",
-            "effect": True,
-            "effect_list": list([
+            "icon":  "mdi:transfer-right",
+            "options": list([
                 "Add",
                 "Dissolve",
                 "Push",
@@ -140,7 +118,52 @@ class MQTT_HASS(Integration):
                 "name": "LedFx",
                 "model": "BladeMOD",
                 "manufacturer": "Yeon",
-                "sw_version": "0.9.0"
+                "sw_version": self._ledfx.config['configuration_version']
+            }
+        }))
+
+
+        # TRANSITION TIME
+        client.subscribe(f"{self._config['topic']}/number/ledfxtransitiontime/set")
+        client.publish(f"{self._config['topic']}/number/ledfxtransitiontime/config", json.dumps({
+            "~": f"{self._config['topic']}/number/ledfxtransitiontime",
+            "name": "3 Transition Time",
+            "unique_id": "ledfxtransitiontime",
+            "cmd_t": "~/set",
+            "stat_t": "~/state",
+            "icon":  "mdi:camera-timer",
+            "min": 0,
+            "max": 10,
+            "step": 0.1,
+            "device": {
+                "identifiers": [
+                    "yzlights"
+                ],
+                "name": "LedFx",
+                "model": "BladeMOD",
+                "manufacturer": "Yeon",
+                "sw_version": self._ledfx.config['configuration_version']
+            }
+        }))
+
+
+        # SWITCH
+        client.subscribe(f"{self._config['topic']}/switch/ledfxplay/set")
+        client.publish(f"{self._config['topic']}/switch/ledfxplay/config", json.dumps({
+            "~": f"{self._config['topic']}/switch/ledfxplay",
+            "name": "4 Play / Pause",
+            "unique_id": "ledfxplay",
+            "cmd_t": "~/set",
+            "stat_t": "~/state",
+            "icon":  "mdi:play-pause",
+            "device": {
+                "identifiers": [
+                    "yzlights"
+                ],
+                "name": "LedFx",
+                "model": "BladeMOD",
+                "manufacturer": "Yeon",
+                "sw_version": self._ledfx.config['configuration_version']
             }
         }))
 
@@ -183,62 +206,34 @@ class MQTT_HASS(Integration):
             virtualid=segs[2]
             _LOGGER.warning(f"YZ{virtualid}")
 
-            ### React to Transition-Config
-            if virtualid == "ledfxtransition":
+            ### React to Transition-Type
+            if virtualid == "ledfxtransitiontype":
                 _LOGGER.info("Transitions: " + str(msg.payload))
-                mydict = ast.literal_eval(msg.payload.decode('utf-8'))
-                if "effect" in mydict:
-                    _LOGGER.info("Change Transition Effect: " + mydict["effect"])
-                    ### Todo: set Global Transition Effect to mydict["effect"]
-                if "brightness" in mydict:
-                    _LOGGER.info("Change Transition Time to " + str(mydict["brightness"]*10) + "ms")
-                    ### Todo: set Global Transition Time to mydict["brightness"]*10 ms
+                # mydict = ast.literal_eval(msg.payload.decode('utf-8'))
+                # if "effect" in mydict:
+                #     _LOGGER.info("Change Transition Effect: " + mydict["effect"])
+                #     ### Todo: set Global Transition Effect to mydict["effect"]
+                # if "brightness" in mydict:
+                #     _LOGGER.info("Change Transition Time to " + str(mydict["brightness"]*10) + "ms")
+                #     ### Todo: set Global Transition Time to mydict["brightness"]*10 ms
                 client.publish(f"{self._config['topic']}/light/{virtualid}/state", msg.payload)
                 return
 
-            ### React to Scene-Selector
-            if virtualid == "ledfxscene":
-                mydict = ast.literal_eval(msg.payload.decode('utf-8'))                
-                if "effect" in mydict:
-                    scene_id = mydict["effect"]
-                    ### SET SCENE ###
-                    if scene_id is None:
-                        response = {
-                            "status": "failed",
-                            "reason": 'Required attribute "scene_id" was not provided',
-                        }
-                        return _LOGGER.warning(response)
-                    _LOGGER.warning(str(self._ledfx.config["scenes"].keys()))
-                    if scene_id not in self._ledfx.config["scenes"].keys():
-                        response = {
-                            "status": "failed",
-                            "reason": f'Scene "{scene_id}" does not exist',
-                        }
-                        return _LOGGER.warning(response)
 
-                    scene = self._ledfx.config["scenes"][scene_id]
 
-                    for virtual in self._ledfx.virtuals.values():
-                        if virtual.id not in scene["virtuals"].keys():
-                            _LOGGER.warning(
-                                ("virtual with id {} has no data in scene {}").format(
-                                    virtual.id, scene_id
-                                )
-                            )
-                            continue
+            ### React to Transition-Time
+            if virtualid == "ledfxtransitiontime":
+                _LOGGER.info("Transitions: " + str(msg.payload))
+                # mydict = ast.literal_eval(msg.payload.decode('utf-8'))
+                # if "effect" in mydict:
+                #     _LOGGER.info("Change Transition Effect: " + mydict["effect"])
+                #     ### Todo: set Global Transition Effect to mydict["effect"]
+                # if "brightness" in mydict:
+                #     _LOGGER.info("Change Transition Time to " + str(mydict["brightness"]*10) + "ms")
+                #     ### Todo: set Global Transition Time to mydict["brightness"]*10 ms
+                client.publish(f"{self._config['topic']}/light/{virtualid}/state", msg.payload)
+                return
 
-                        if scene["virtuals"][virtual.id]:
-                            effect = self._ledfx.effects.create(
-                                ledfx=self._ledfx,
-                                type=scene["virtuals"][virtual.id]["type"],
-                                config=scene["virtuals"][virtual.id]["config"],
-                            )
-                            virtual.set_effect(effect)
-                        else:
-                            virtual.clear_effect()
-
-                    self._ledfx.events.fire_event(SceneSetEvent(scene["name"]))
-                    ### SET SCENE END ###
 
 
             ### React to Scene-Selector-v2
@@ -365,6 +360,9 @@ class MQTT_HASS(Integration):
         self._client.publish(f"{self._config['topic']}/light/ledfxscene/config", json.dumps({}))
         self._client.publish(f"{self._config['topic']}/light/ledfxtransition/config", json.dumps({}))
         self._client.publish(f"{self._config['topic']}/select/ledfxsceneselect/config", json.dumps({}))        
+        self._client.publish(f"{self._config['topic']}/select/ledfxtransitiontype/config", json.dumps({}))        
+        self._client.publish(f"{self._config['topic']}/number/ledfxtransitiontime/config", json.dumps({}))        
+        self._client.publish(f"{self._config['topic']}/switch/ledfxplay/config", json.dumps({}))        
         for virtual in self._ledfx.virtuals.values():
             self._client.publish(f"{self._config['topic']}/light/{virtual.id}/config", json.dumps({}))
 
