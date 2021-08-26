18e40199d512c1d4cc598c570b9c714df3bec59a
Matthew Bowley <m.bowley98@gmail.com>
2021-08-26 21:45:58 +0100

    new strobe effect (todo: ledfx presets)

--- ledfx/effects/strobe.py
+++ ledfx/effects/strobe.py
@@ -1,13 +1,15 @@
 import numpy as np
 import voluptuous as vol
 
-from ledfx.color import COLORS
+from ledfx.color import COLORS, GRADIENTS
 from ledfx.effects.audio import AudioReactiveEffect
+from ledfx.effects.gradient import GradientEffect
 
 
-class Strobe(AudioReactiveEffect):
+class Strobe(AudioReactiveEffect, GradientEffect):
 
     MAPPINGS = {
+        "1/1 (.,. )": 1,
         "1/2 (.-. )": 2,
         "1/4 (.o. )": 4,
         "1/8 (◉◡◉ )": 8,
@@ -21,29 +23,61 @@ class Strobe(AudioReactiveEffect):
     CONFIG_SCHEMA = vol.Schema(
         {
             vol.Optional(
-                "color", description="Strobe colour", default="white"
+                "color",
+                description="Strobe colour (used if single_colour selected)",
+                default="white",
             ): vol.In(list(COLORS.keys())),
             vol.Optional(
-                "frequency",
-                description="Strobe frequency",
+                "gradient_name",
+                description="Cycle through a gradient each beat",
+                default="Rainbow",
+            ): vol.In(list(GRADIENTS.keys())),
+            vol.Optional(
+                "single_color",
+                description="Display a single colour strobe",
+                default=False,
+            ): bool,
+            vol.Optional(
+                "strobe_frequency",
+                description="How many strobes per beat",
                 default=list(MAPPINGS.keys())[1],
             ): vol.In(list(MAPPINGS.keys())),
+            vol.Optional(
+                "strobe_decay",
+                description="How rapidly a single strobe hit fades. Higher -> faster fade",
+                default=1.5,
+            ): vol.All(vol.Coerce(float), vol.Range(min=1, max=10)),
+            vol.Optional(
+                "beat_decay",
+                description="How much the strobes fade across the beat. Higher -> less bright strobes towards end of beat",
+                default=2,
+            ): vol.All(vol.Coerce(float), vol.Range(min=0, max=10)),
         }
     )
 
     def on_activate(self, pixel_count):
         self.output = np.zeros((pixel_count, 3))
+        self.color = self.get_gradient_color(0)
         self.brightness = 0
 
     def config_updated(self, config):
-        self.color = np.array(COLORS[self._config["color"]], dtype=float)
-        self.f = self.MAPPINGS[self._config["frequency"]]
+        self.single_color = np.array(
+            COLORS[self._config["color"]], dtype=float
+        )
+        self.freq = self.MAPPINGS[self._config["strobe_frequency"]]
+        self.strobe_decay = self._config["strobe_decay"]
+        self.beat_decay = self._config["beat_decay"]
 
     def audio_data_updated(self, data):
-        self.brightness = (-data.beat_oscillator() % (2 / self.f)) * (
-            self.f / 2
-        )
+        o = data.beat_oscillator()
+        self.color = self.get_gradient_color(data.bar_oscillator() / 4)
+
+        self.brightness = (
+            ((-o % (1 / self.freq)) * self.freq) ** self.strobe_decay
+        ) * (1 - o) ** self.beat_decay
 
     def render(self):
-        self.output[:] = self.color * self.brightness
+        self.output[:] = (
+            self.single_color if self._config["single_color"] else self.color
+        ) * self.brightness
         return self.output
