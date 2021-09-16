9f20ee0168226fe182dd6fc47333b55b9b1a5e36
Matthew Bowley <m.bowley98@gmail.com>
2021-09-16 19:28:22 +0100

    Update audio.py

--- ledfx/effects/audio.py
+++ ledfx/effects/audio.py
@@ -224,7 +224,9 @@ class AudioInputSource:
                 ACTIVE_AUDIO_STREAM = self._stream = WebAudioStream(
                     input_device["client"], self._audio_sample_callback
                 )
-                _LOGGER.info("Opened Web Audio Stream")
+                _LOGGER.info(
+                    f"Opened Web Audio Stream: {input_device['client']}"
+                )
             else:
                 self._stream = self._audio.InputStream(
                     samplerate=self._config["mic_rate"],
