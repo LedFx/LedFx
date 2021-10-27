c5330ac610a2f3515732e97e56eee767f49b4e0b
thatdonfc <thatdonfc+git@gmail.com>
2021-10-27 12:00:39 -0700

    Fix WLED<0.13.0 devices would not be created
    # WLED devices < v0.13.0 failed to be created after performing a wled scan

--- ledfx/devices/wled.py
+++ ledfx/devices/wled.py
@@ -1,4 +1,5 @@
 import logging
+from distutils.version import LooseVersion
 
 import voluptuous as vol
 
@@ -104,7 +105,6 @@ class WLEDDevice(NetworkedDevice):
         await super().async_initialize()
         self.wled = WLED(self.destination)
         wled_config = await self.wled.get_config()
-        await self.wled.get_sync_settings()
 
         led_info = wled_config["leds"]
         wled_name = wled_config["name"]
@@ -123,10 +123,11 @@ class WLEDDevice(NetworkedDevice):
 
         # Currently *assuming* that this PR gets released in 0.13
         # https://github.com/Aircoookie/WLED/pull/1944
-        # if StrictVersion(wled_version) >= StrictVersion("0.13.0"):
-        #     _LOGGER.info(
-        #         f"WLED Version Supports Sync Setting API: {wled_version}"
-        #     )
+        if LooseVersion(wled_version) >= LooseVersion("0.13.0"):
+            _LOGGER.info(
+                f"WLED Version Supports Sync Setting API: {wled_version}"
+            )
+            wled_sync_settings = await self.wled.get_sync_settings()
         # self.wled.enable_realtime_gamma()
         # self.wled.set_inactivity_timeout(self._config["timeout"])
         # self.wled.first_universe()
