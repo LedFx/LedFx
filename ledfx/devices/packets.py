4aed716c876eca0748ec332386837aeff2e49568
WeekendWarrior1 <c.halton.93@gmail.com>
2021-11-06 21:43:50 +1100

    Basic openrgb (tcp) device, clean up packets.py

--- ledfx/devices/packets.py
+++ ledfx/devices/packets.py
@@ -1,19 +1,20 @@
+import struct
+
 import numpy as np
-
-"""
-Generic WARLS packet encoding
-Max LEDs: 255
-
-Header: [1, timeout]
-Byte 	Description
-2 + n*4 	LED Index
-3 + n*4 	Red Value
-4 + n*4 	Green Value
-5 + n*4 	Blue Value
-"""
 
 
 def build_warls_packet(data: np.ndarray, timeout: int, last_frame: np.array):
+    """
+    Generic WARLS packet encoding
+    Max LEDs: 255
+
+    Header: [1, timeout]
+    Byte 	Description
+    2 + n*4 	LED Index
+    3 + n*4 	Red Value
+    4 + n*4 	Green Value
+    5 + n*4 	Blue Value
+    """
     packet = bytearray([1, (timeout or 1)])
 
     byteData = data.astype(np.dtype("B"))
@@ -39,20 +40,18 @@ def build_warls_packet(data: np.ndarray, timeout: int, last_frame: np.array):
     return packet
 
 
-"""
-Generic DRGB packet encoding
-Max LEDs: 490
-
-Header: [2, timeout]
-Byte 	Description
-2 + n*3 	Red Value
-3 + n*3 	Green Value
-4 + n*3 	Blue Value
-
-"""
-
-
 def build_drgb_packet(data: np.ndarray, timeout: int):
+    """
+    Generic DRGB packet encoding
+    Max LEDs: 490
+
+    Header: [2, timeout]
+    Byte 	Description
+    2 + n*3 	Red Value
+    3 + n*3 	Green Value
+    4 + n*3 	Blue Value
+
+    """
     packet = bytearray([2, (timeout or 1)])
 
     byteData = data.astype(np.dtype("B"))
@@ -60,20 +59,18 @@ def build_drgb_packet(data: np.ndarray, timeout: int):
     return packet
 
 
-"""
-Generic DRGBW packet encoding
-Max LEDs: 367
-
-Header: [3, timeout]
-Byte 	Description
-2 + n*3 	Red Value
-3 + n*3 	Green Value
-4 + n*3 	Blue Value
-5 + n*4 	White Value
-"""
-
-
 def build_drgbw_packet(data: np.ndarray, timeout: int):
+    """
+    Generic DRGBW packet encoding
+    Max LEDs: 367
+
+    Header: [3, timeout]
+    Byte 	Description
+    2 + n*3 	Red Value
+    3 + n*3 	Green Value
+    4 + n*3 	Blue Value
+    5 + n*4 	White Value
+    """
     packet = bytearray([3, (timeout or 1)])
 
     byteData = data.astype(np.dtype("B"))
@@ -88,21 +85,19 @@ def build_drgbw_packet(data: np.ndarray, timeout: int):
     return packet
 
 
-"""
-Generic DNRGB packet encoding
-Max LEDs: 489 / packet
-
-Header: [4, timeout, start index high byte, start index low byte]
-Byte 	Description
-4 + n*3 	Red Value
-5 + n*3 	Green Value
-6 + n*3 	Blue Value
-"""
-
-
 def build_dnrgb_packet(
     data: np.ndarray, timeout: int, led_start_index: np.uint16
 ):
+    """
+    Generic DNRGB packet encoding
+    Max LEDs: 489 / packet
+
+    Header: [4, timeout, start index high byte, start index low byte]
+    Byte 	Description
+    4 + n*3 	Red Value
+    5 + n*3 	Green Value
+    6 + n*3 	Blue Value
+    """
     packet = bytearray(
         [4, (timeout or 1), (led_start_index >> 8), (led_start_index & 0x00FF)]
     )  # high byte, then low byte
@@ -112,18 +107,16 @@ def build_dnrgb_packet(
     return packet
 
 
-"""
-Generic Adalight serial packet encoding
-
-Header: [A, d, a, pixel count high byte, pixel count low byte, pixel count checksum]
-Byte 	Description
-4 + n*3 	Red Value
-5 + n*3 	Green Value
-6 + n*3 	Blue Value
-"""
-
-
 def build_adalight_packet(data: np.ndarray, color_order: str):
+    """
+    Generic Adalight serial packet encoding
+
+    Header: [A, d, a, pixel count high byte, pixel count low byte, pixel count checksum]
+    Byte 	Description
+    4 + n*3 	Red Value
+    5 + n*3 	Green Value
+    6 + n*3 	Blue Value
+    """
     pixel_length = len(data)
     packet = bytearray(
         [
@@ -151,4 +144,42 @@ def build_adalight_packet(data: np.ndarray, color_order: str):
         byteData[:, [2, 0]] = byteData[:, [0, 2]]
         byteData[:, [1, 0]] = byteData[:, [0, 1]]
     packet.extend(byteData.flatten().tobytes())
+    return packet
+
+
+def build_openrgb_packet(
+    data: np.ndarray,
+    device_id: int,
+):
+    """
+    openRGB packet encoding
+
+    Header: [O,R,G,B, openrgb device_id, RGBCONTROLLER_UPDATELEDS command (1050), total packet bytes length, length of payload, number of pixels]
+    Byte 	Description
+    23 + n*4 	Red Value
+    24 + n*4 	Green Value
+    25 + n*4 	Blue Value
+    26 + n*4 	White Value
+    """
+    frame_size = len(data)
+
+    # header
+    packet = bytearray(
+        # fmt: off
+        struct.pack(
+            "ccccIIIIH",
+            b"O", b"R", b"G", b"B",
+            device_id,
+            1050,  # RGBCONTROLLER_UPDATELEDS packet
+            struct.calcsize(f"IH{3*frame_size}b{frame_size}x"),   # total packet length
+            (frame_size * 4 + 2),  # body length
+            frame_size,  # number of pixels
+        )
+        # fmt: on
+    )
+
+    # body
+    out = np.zeros((frame_size, 4), dtype="B")
+    out[:, 0:3] = data.astype(np.dtype("B"))
+    packet.extend(out.flatten().tobytes())
     return packet
