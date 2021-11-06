3e25509ea00894c3af9e9de9efbe80922e36b645
Shaun Eccles-Smith <shauneccles@gmail.com>
2021-11-06 18:36:22 +1100

    New device-status endpoint & spelling fix

---
+++ ledfx/api/device_status.py
@@ -0,0 +1,21 @@
+import logging
+
+from aiohttp import web
+
+from ledfx.api import RestEndpoint
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class DeviceStatusEndpoint(RestEndpoint):
+
+    ENDPOINT_PATH = "/api/device-status"
+
+    async def get(self) -> web.Response:
+        response = {"active": [], "inactive": []}
+        for virtual in self._ledfx.virtuals.values():
+            if virtual.active_effect:
+                response["active"].append(virtual.id)
+            else:
+                response["inactive"].append(virtual.id)
+        return web.json_response(data=response, status=200)
