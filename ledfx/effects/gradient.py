0cc29f5d492455bd9cf4167a4377ece72ebf0754
YeonV <trusted.yeon@gmail.com>
2021-12-01 01:36:54 +0100

    gradients: set angle to 90; default values

--- ledfx/effects/gradient.py
+++ ledfx/effects/gradient.py
@@ -24,7 +24,7 @@ class GradientEffect(Effect):
             vol.Optional(
                 "gradient",
                 description="Color gradient to display",
-                default="Rainbow",
+                default="linear-gradient(90deg, rgb(255, 0, 0) 0%, rgb(255, 120, 0) 14%, rgb(255, 200, 0) 28%, rgb(0, 255, 0) 42%, rgb(0, 199, 140) 56%, rgb(0, 0, 255) 70%, rgb(128, 0, 128) 84%, rgb(255, 0, 178) 98%)",
             ): validate_gradient,
             vol.Optional(
                 "gradient_roll",
--- ledfx/effects/multiBar.py
+++ ledfx/effects/multiBar.py
@@ -16,7 +16,7 @@ class MultiBarAudioEffect(AudioReactiveEffect, GradientEffect):
             vol.Optional(
                 "gradient_name",
                 description="Color scheme to cycle through",
-                default="Rainbow",
+                default="linear-gradient(90deg, rgb(255, 0, 0) 0%, rgb(255, 120, 0) 14%, rgb(255, 200, 0) 28%, rgb(0, 255, 0) 42%, rgb(0, 199, 140) 56%, rgb(0, 0, 255) 70%, rgb(128, 0, 128) 84%, rgb(255, 0, 178) 98%)",
             ): vol.In(list(GRADIENTS.keys())),
             vol.Optional(
                 "mode",
