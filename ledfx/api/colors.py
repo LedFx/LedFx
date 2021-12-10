510840f4073ab75f584be47341f46565b884e0b6
Matthew Bowley <m.bowley98@gmail.com>
2021-12-10 16:14:11 +0000

    Merge color/gradient api

--- ledfx/api/colors.py
+++ ledfx/api/colors.py
@@ -13,28 +13,44 @@ class ColorEndpoint(RestEndpoint):
 
     async def get(self) -> web.Response:
         """
-        Get LedFx colors.
+        Get LedFx colors and gradients
         """
-        response = dict(zip(("builtin", "user"), self._ledfx.colors.get_all()))
+        response = {
+            "colors": dict(
+                zip(("builtin", "user"), self._ledfx.colors.get_all())
+            ),
+            "gradients": dict(
+                zip(("builtin", "user"), self._ledfx.gradients.get_all())
+            ),
+        }
 
         return web.json_response(data=response, status=200)
 
     async def delete(self, request) -> web.Response:
         """
-        Deletes a user defined color
+        Deletes a user defined color or gradient
         Request body is a string of the color key to delete
-        eg. "my_red_color"
+        eg. {
+            "colors": ["my_red_color"],
+        }
         """
 
-        key = await request.json()
+        data = await request.json()
 
-        del self._ledfx.colors[key]
+        colors = data.get("colors")
+        gradients = data.get("gradients")
+
+        for key in colors:
+            del self._ledfx.colors[key]
+
+        for key in gradients:
+            del self._ledfx.gradients[key]
 
         response = {
             "status": "success",
             "payload": {
                 "type": "success",
-                "reason": f"Deleted color {key}",
+                "reason": "Deleted",
             },
         }
 
@@ -42,20 +58,29 @@ class ColorEndpoint(RestEndpoint):
 
     async def post(self, request) -> web.Response:
         """
-        Creates or updates existing color.
-        eg. {"my_red_color": "#ffffff"}
+        Creates or updates existing colors or gradients.
+        eg. {
+            "colors: {"my_red_color": "#ffffff"},
+            "gradients": {"my_cool_gradient": "lin..."}
+        }
         """
 
-        colors = await request.json()
+        data = await request.json()
+
+        colors = data.get("colors")
+        gradients = data.get("gradients")
 
         for key, val in colors.items():
             self._ledfx.colors[key] = val
+
+        for key, val in gradients.items():
+            self._ledfx.gradients[key] = val
 
         response = {
             "status": "success",
             "payload": {
                 "type": "success",
-                "reason": f"Saved color{'s' if len(colors) > 1 else ''}",
+                "reason": "Saved",
             },
         }
 
