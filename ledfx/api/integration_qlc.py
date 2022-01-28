9ac1aacae6ea062c0276bf88490c321fd360c46a
Mattallmighty <matt4788667@hotmail.com>
2022-01-28 20:13:46 +1100

    Restore QLC API

--- ledfx/api/integration_qlc.py
+++ ledfx/api/integration_qlc.py
@@ -1,4 +1,5 @@
 import logging
+from json import JSONDecodeError
 
 from aiohttp import web
 
@@ -14,59 +15,42 @@ class QLCEndpoint(RestEndpoint):
 
     ENDPOINT_PATH = "/api/integrations/qlc/{integration_id}"
 
-    async def get(self, integration_id, request) -> web.Response:
+    async def get(self, integration_id) -> web.Response:
         """Get info from QLC+ integration"""
         integration = self._ledfx.integrations.get(integration_id)
         if (integration is None) or (integration.type != "qlc"):
             response = {"not found": 404}
             return web.json_response(data=response, status=404)
 
-        data = await request.json()
-        info = data.get("info")
-        if info is None:
-            response = {
-                "status": "failed",
-                "reason": 'Required attribute "info" was not provided',
-            }
-            return web.json_response(data=response, status=500)
+        response = {}
 
-        if info == "event_types":
-            # generate dict of {effect_id: effect_name}
-            effect_names = []
-            for effect_type, effect in self._ledfx.effects.classes().items():
-                effect_names.append(effect.NAME)
+        # generate dict of {effect_id: effect_name}
+        effect_names = []
+        for effect_type, effect in self._ledfx.effects.classes().items():
+            effect_names.append(effect.NAME)
 
-            scene_names = []
-            for scene in self._ledfx.config["scenes"]:
-                scene_names.append(self._ledfx.config["scenes"][scene]["name"])
+        scene_names = []
+        for scene in self._ledfx.config["scenes"]:
+            scene_names.append(self._ledfx.config["scenes"][scene]["name"])
 
-            response = {
-                Event.EFFECT_SET: {
-                    "event_name": "Effect Set",
-                    "event_filters": {"effect_name": effect_names},
-                },
-                Event.EFFECT_CLEARED: {
-                    "event_name": "Effect Cleared",
-                    "event_filters": {},
-                },
-                Event.SCENE_ACTIVATED: {
-                    "event_name": "Scene Activated",
-                    "event_filters": {"scene_name": scene_names},
-                },
-            }
+        response["event_types"] = {
+            Event.EFFECT_SET: {
+                "event_name": "Effect Set",
+                "event_filters": {"effect_name": effect_names},
+            },
+            Event.EFFECT_CLEARED: {
+                "event_name": "Effect Cleared",
+                "event_filters": {},
+            },
+            Event.SCENE_ACTIVATED: {
+                "event_name": "Scene Set",
+                "event_filters": {"scene_name": scene_names},
+            },
+        }
 
-        elif info == "qlc_widgets":
-            response = await integration.get_widgets()
+        response["qlc_widgets"] = await integration.get_widgets()
 
-        elif info == "qlc_listeners":
-            response = integration.data
-
-        else:
-            response = {
-                "status": "failed",
-                "reason": f'Unknown info parameter "{info}"',
-            }
-            return web.json_response(data=response, status=500)
+        response["qlc_listeners"] = integration.data
 
         return web.json_response(data=response, status=200)
 
@@ -77,7 +61,14 @@ class QLCEndpoint(RestEndpoint):
             response = {"not found": 404}
             return web.json_response(data=response, status=404)
 
-        data = await request.json()
+        try:
+            data = await request.json()
+        except JSONDecodeError:
+            response = {
+                "status": "failed",
+                "reason": "JSON Decoding failed",
+            }
+            return web.json_response(data=response, status=400)
         event_type = data.get("event_type")
         event_filter = data.get("event_filter")
 
@@ -86,21 +77,21 @@ class QLCEndpoint(RestEndpoint):
                 "status": "failed",
                 "reason": 'Required attribute "event_type" was not provided',
             }
-            return web.json_response(data=response, status=500)
+            return web.json_response(data=response, status=400)
 
         if event_filter is None:
             response = {
                 "status": "failed",
                 "reason": 'Required attribute "event_filter" was not provided',
             }
-            return web.json_response(data=response, status=500)
+            return web.json_response(data=response, status=400)
 
         if type(event_filter) is not dict:
             response = {
                 "status": "failed",
                 "reason": f'Invalid filter "{event_filter}", should be dictionary eg. {{ "scene_name" : "my scene" }} ',
             }
-            return web.json_response(data=response, status=500)
+            return web.json_response(data=response, status=400)
 
         # toggle the event listener
         if not integration.toggle_event(event_type, event_filter):
@@ -108,7 +99,7 @@ class QLCEndpoint(RestEndpoint):
                 "status": "failed",
                 "reason": f"Could not find event with type {event_type} and filter {event_filter}",
             }
-            return web.json_response(data=response, status=500)
+            return web.json_response(data=response, status=400)
 
         # Save the configuration (integration will handle modifying "data")
         for _integration in self._ledfx.config["integrations"]:
@@ -130,7 +121,14 @@ class QLCEndpoint(RestEndpoint):
             response = {"not found": 404}
             return web.json_response(data=response, status=404)
 
-        data = await request.json()
+        try:
+            data = await request.json()
+        except JSONDecodeError:
+            response = {
+                "status": "failed",
+                "reason": "JSON Decoding failed",
+            }
+            return web.json_response(data=response, status=400)
         event_type = data.get("event_type")
         event_filter = data.get("event_filter")
         qlc_payload = data.get("qlc_payload")
@@ -140,28 +138,28 @@ class QLCEndpoint(RestEndpoint):
                 "status": "failed",
                 "reason": 'Required attribute "event_type" was not provided',
             }
-            return web.json_response(data=response, status=500)
+            return web.json_response(data=response, status=400)
 
         if event_filter is None:
             response = {
                 "status": "failed",
                 "reason": 'Required attribute "event_filter" was not provided',
             }
-            return web.json_response(data=response, status=500)
+            return web.json_response(data=response, status=400)
 
         if type(event_filter) is not dict:
             response = {
                 "status": "failed",
                 "reason": f'Invalid filter "{event_filter}", should be dictionary eg. {{ "scene_name" : "my scene" }} ',
             }
-            return web.json_response(data=response, status=500)
+            return web.json_response(data=response, status=400)
 
         if qlc_payload is None:
             response = {
                 "status": "failed",
                 "reason": 'Required attribute "qlc_payload" was not provided',
             }
-            return web.json_response(data=response, status=500)
+            return web.json_response(data=response, status=400)
 
         # Create a link between ledfx event and sending the payload
         integration.create_event(event_type, event_filter, True, qlc_payload)
@@ -186,7 +184,14 @@ class QLCEndpoint(RestEndpoint):
             response = {"not found": 404}
             return web.json_response(data=response, status=404)
 
-        data = await request.json()
+        try:
+            data = await request.json()
+        except JSONDecodeError:
+            response = {
+                "status": "failed",
+                "reason": "JSON Decoding failed",
+            }
+            return web.json_response(data=response, status=400)
         event_type = data.get("event_type")
         event_filter = data.get("event_filter")
 
@@ -195,21 +200,21 @@ class QLCEndpoint(RestEndpoint):
                 "status": "failed",
                 "reason": 'Required attribute "event_type" was not provided',
             }
-            return web.json_response(data=response, status=500)
+            return web.json_response(data=response, status=400)
 
         if event_filter is None:
             response = {
                 "status": "failed",
                 "reason": 'Required attribute "event_filter" was not provided',
             }
-            return web.json_response(data=response, status=500)
+            return web.json_response(data=response, status=400)
 
         if type(event_filter) is not dict:
             response = {
                 "status": "failed",
                 "reason": f'Invalid filter "{event_filter}", should be dictionary eg. {{ "scene_name" : "my scene" }} ',
             }
-            return web.json_response(data=response, status=500)
+            return web.json_response(data=response, status=400)
 
         # Delete the listener and event from data
         integration.delete_event(event_type, event_filter)
