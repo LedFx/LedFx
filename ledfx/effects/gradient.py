Fix gradient roll
Shifting axis=0 makes it look more like a strobe, axis=1 produces expected result

ledfx/effects/gradient.py
@@ -138,7 +138,7 @@ class GradientEffect(Effect):
             self._gradient_curve = np.roll(
                 self._gradient_curve,
                 int(pixels_to_roll),
-                axis=0,
+                axis=1,
             )
 
     def get_gradient_color(self, point):