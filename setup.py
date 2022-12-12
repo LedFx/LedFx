64009f943731d77b8d880844f8072b84c59992c7
shauneccles <shauneccles@gmail.com>
2022-12-12 19:30:47 +1100

    Remove yappi

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ INSTALL_REQUIRES = [
     "samplerate>=0.1.0",
     "tcp-latency>=0.0.10",
     "voluptuous~=0.12.1",
-    "yappi~=1.3.3",
     "zeroconf==0.39.4",
     "pillow>=8.4.0",
     # Conditional Requirement
