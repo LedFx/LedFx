c276a7b06b4bb9607d2babd6b93ff4ecdf75a9b8
YeonV <trusted.yeon@gmail.com>
2022-12-26 00:40:17 +0100

    Delete old frontend code

--- frontend/src/routes/index.jsx
+++
@@ -1,5 +0,0 @@
-import DefaultLayout from "../layouts/Default";
-
-const indexRoutes = [{ path: "/", component: DefaultLayout }];
-
-export default indexRoutes;
--- frontend/src/utils/api/index.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const baseURL = 'http://localhost:8888/api';
-
-export const api = axios.create({
-    baseURL,
-});
