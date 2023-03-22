51c17e6ff98949d7c5802374cbc7eb0b249d8676
bigredfrog <anthony.tod@gmail.com>
2023-03-22 12:08:32 -0400

    Delete bits.py

--- bits.py
+++
@@ -1,9 +0,0 @@
-# HOW TO DEBUG METHODS
-
-_LOGGER.info(" - Available methods:")
-for mName in sorted(dir(effect)):
-    if mName.find("__") >= 0:
-        continue
-
-    if callable(getattr(effect, mName)):
-        _LOGGER.info(f"     {mName}()")
