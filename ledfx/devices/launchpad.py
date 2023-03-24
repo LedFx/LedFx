989e35d02e77edf964bd5a0912c666b110ceabd0
atod <anthony.tod@gmail.com>
2023-03-24 17:26:26 -0400

    Improved abstraction slightly for flush_launchpad so we can add new versions

--- ledfx/devices/launchpad.py
+++ ledfx/devices/launchpad.py
@@ -26,65 +26,6 @@ def dump_methods(lp_instance, device_type):
             _LOGGER.debug(f"     {mName}()")
 
 
-def validate_launchpad(lp_instance):
-    if lp_instance is None:
-        lp_instance = launchpad.Launchpad()
-
-    # try the first Mk2
-    if lp_instance.Check(0, "mk2"):
-        lp_instance = launchpad.LaunchpadMk2()
-        if lp_instance.Open(0, "mk2"):
-            _LOGGER.info(" - Launchpad Mk2: OK")
-        else:
-            _LOGGER.error(" - Launchpad Mk2: ERROR")
-            return
-
-    # try the first Mini Mk3
-    elif lp_instance.Check(1, "minimk3"):
-        lp_instance = launchpad.LaunchpadMiniMk3()
-        if lp_instance.Open(1, "minimk3"):
-            _LOGGER.info(" - Launchpad Mini Mk3: OK")
-        else:
-            _LOGGER.error(" - Launchpad Mini Mk3: ERROR")
-            return
-
-    # try the first Pro
-    elif lp_instance.Check(0, "pad pro"):
-        lp_instance = launchpad.LaunchpadPro()
-        if lp_instance.Open(0, "pad pro"):
-            _LOGGER.info(" - Launchpad Pro: OK")
-        else:
-            _LOGGER.error(" - Launchpad Pro: ERROR")
-            return
-
-    # try the first Pro Mk3
-    elif lp_instance.Check(0, "promk3"):
-        lp_instance = launchpad.LaunchpadProMk3()
-        if lp_instance.Open(0):
-            _LOGGER.info(" - Launchpad Pro Mk3: OK")
-        else:
-            _LOGGER.error(" - Launchpad Pro Mk3: ERROR")
-            return
-
-    # try the first X
-    # Notice that this is already built-in in the LPX class' methods Check() and Open,
-    # but we're using the one from above!
-    elif lp_instance.Check(1, "Launchpad X") or lp_instance.Check(1, "LPX"):
-        lp_instance = launchpad.LaunchpadLPX()
-        # Open() includes looking for "LPX" and "Launchpad X"
-        if lp_instance.Open(1):
-            _LOGGER.info(" - Launchpad X: OK")
-        else:
-            _LOGGER.error(" - Launchpad X: ERROR")
-            return
-
-    # nope
-    else:
-        _LOGGER.error(" - No Launchpad available")
-        return
-    return lp_instance
-
-
 class LaunchpadDevice(MidiDevice):
     """Launchpad device support"""
 
@@ -113,12 +54,25 @@ class LaunchpadDevice(MidiDevice):
 
     def __init__(self, ledfx, config):
         super().__init__(ledfx, config)
+        self.lp = None
+        self.flush_launchpad = None
 
     def flush(self, data):
-        try:
-            # count = 0
-            # tooks = []
+        self.flush_launchpad(data)
 
+    def activate(self):
+        self.lp = launchpad.Launchpad()
+        self.validate_launchpad()
+        super().activate()
+
+    def deactivate(self):
+        self.lp.Reset()
+        self.lp.Close()
+        super().deactivate()
+
+    # Need a flush variant for each supported Launchpad, and assign in validate
+    def flush_launchpadLPX(self, data):
+        try:
             # we will use RawWriteSysEx(self, lstMessage, timeStamp=0)
             # this function adds the preamble 240 and post amble 247
             #
@@ -153,15 +107,14 @@ class LaunchpadDevice(MidiDevice):
             # (between dim and bright green), and the pad next to that pulsing turquoise
             #
             # in summary
+            # [ 3 = RGB, Pos = layout BE CAREFUL, R,G, B max 127 ]
+            # example of send RED pixel at row 3 pixel 6
+            # send_buffer.extend([3, 35, 127, 0, 0])
 
             # start = timeit.default_timer()
 
             # stuff the send buffer with the command preamble
             send_buffer = [0, 32, 41, 2, 12, 3]
-
-            # [ 3 = RGB, Pos = layout BE CAREFUL, R,G, B max 127 ]
-            # example of send RED pixel at row 3 pixel 6
-            # send_buffer.extend([3, 35, 127, 0, 0])
 
             # prebump the programmer mode index up a row and just before
             pgm_mode_pos = 10
@@ -184,14 +137,60 @@ class LaunchpadDevice(MidiDevice):
             # _LOGGER.info(f"Updated Pixels: {took} ")
 
         except RuntimeError:
-            _LOGGER.error("Error in Launchpad handling")
+            _LOGGER.error("Error in LaunchpadLPX handling")
 
-    def activate(self):
-        self.lp = launchpad.Launchpad()
-        self.lp = validate_launchpad(self.lp)
-        super().activate()
+    def validate_launchpad(self):
+        # try the first Mk2
+        if self.lp.Check(0, "mk2"):
+            self.lp = launchpad.LaunchpadMk2()
+            if self.lp.Open(0, "mk2"):
+                _LOGGER.info(" - Launchpad Mk2: OK")
+            else:
+                _LOGGER.error(" - Launchpad Mk2: ERROR")
+                return
 
-    def deactivate(self):
-        self.lp.Reset()
-        self.lp.Close()
-        super().deactivate()
+        # try the first Mini Mk3
+        elif self.lp.Check(1, "minimk3"):
+            self.lp = launchpad.LaunchpadMiniMk3()
+            if self.lp.Open(1, "minimk3"):
+                _LOGGER.info(" - Launchpad Mini Mk3: OK")
+            else:
+                _LOGGER.error(" - Launchpad Mini Mk3: ERROR")
+                return
+
+        # try the first Pro
+        elif self.lp.Check(0, "pad pro"):
+            self.lp = launchpad.LaunchpadPro()
+            if self.lp.Open(0, "pad pro"):
+                _LOGGER.info(" - Launchpad Pro: OK")
+            else:
+                _LOGGER.error(" - Launchpad Pro: ERROR")
+                return
+
+        # try the first Pro Mk3
+        elif self.lp.Check(0, "promk3"):
+            self.lp = launchpad.LaunchpadProMk3()
+            if self.lp.Open(0):
+                _LOGGER.info(" - Launchpad Pro Mk3: OK")
+            else:
+                _LOGGER.error(" - Launchpad Pro Mk3: ERROR")
+                return
+
+        # try the first X
+        # Notice that this is already built-in in the LPX class' methods Check() and Open,
+        # but we're using the one from above!
+        elif self.lp.Check(1, "Launchpad X") or self.lp.Check(1, "LPX"):
+            self.lp = launchpad.LaunchpadLPX()
+            self.flush_launchpad = self.flush_launchpadLPX
+            # Open() includes looking for "LPX" and "Launchpad X"
+            if self.lp.Open(1):
+                _LOGGER.info(" - Launchpad X: OK")
+            else:
+                _LOGGER.error(" - Launchpad X: ERROR")
+                return
+
+        # nope
+        else:
+            _LOGGER.error(" - No Launchpad available")
+            self.flush_launchpad = None
+            return
