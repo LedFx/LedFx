40cb1720e969368e3526c6f8631f643493091111
atod <anthony.tod@gmail.com>
2023-06-13 22:54:52 -0400

    added server / port override from defaults

--- ledfx/api/find_openrgb.py
+++ ledfx/api/find_openrgb.py
@@ -9,20 +9,30 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class FindOpenRGBDevicesEndpoint(RestEndpoint):
-    """REST end-point for detecting and reporting OpenRGB devices"""
+    """REST end-point for detecting and reporting OpenRGB devices
+       optional params for server and port"""
 
     ENDPOINT_PATH = "/api/find_openrgb"
 
-    async def post(self) -> web.Response:
+    async def get(self, request) -> web.Response:
         """Check for an openrgb server and report devices"""
+
+        if "server" in request.query.keys():
+            server = request.query["server"]
+        else:
+            server = "127.0.0.1"
+
+        if "port" in request.query.keys():
+            port = int(request.query["port"])
+        else:
+            port = 6742
+
         try:
-            client = OpenRGBClient()
+            client = OpenRGBClient(address=server, port=port)
         except Exception as e:
             _LOGGER.error(f"Failed to connect to OpenRGB server: {e}")
             response = {"status": "error", "error": str(e)}
             return web.json_response(data=response, status=500)
-
-        print(client.devices)
 
         devices = []
         for device in client.devices:
