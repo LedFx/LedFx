0a213f54270ea82637767a68a8733d65e6a81d0b
atod <anthony.tod@gmail.com>
2023-11-03 21:14:34 -0400

    Add slider for melbank filter mapping into RGB channels

--- ledfx/effects/spectrum.py
+++ ledfx/effects/spectrum.py
@@ -8,7 +8,16 @@ class SpectrumAudioEffect(AudioReactiveEffect):
     NAME = "Spectrum"
     CATEGORY = "Classic"
 
-    CONFIG_SCHEMA = vol.Schema({})
+    CONFIG_SCHEMA = vol.Schema({
+        vol.Optional(
+            "RGB Mix",
+            description="How the melbank filters are applied to the RGB values",
+            default=0,
+        ): vol.All(vol.Coerce(int), vol.Range(min=0, max=5)),
+    })
+
+    rgb_mixes = [[0, 1, 2], [0, 2, 1], [1, 0, 2],
+                 [1, 2, 0], [2, 0, 1], [2, 1, 0]]
 
     def on_activate(self, pixel_count):
         self.out = np.zeros((pixel_count, 3))
@@ -18,18 +27,20 @@ class SpectrumAudioEffect(AudioReactiveEffect):
         # prevent crashes from segment edit / led count changes
         if self._b_filter is not None:
             self._b_filter.value = None
+        self.rgb_mix = self.rgb_mixes[self._config["RGB Mix"]]
 
     def config_updated(self, config):
         # Create all the filters used for the effect
         self._b_filter = self.create_filter(alpha_decay=0.1, alpha_rise=0.5)
+        self.rgb_mix = self.rgb_mixes[self._config["RGB Mix"]]
 
     def audio_data_updated(self, data):
         # Grab the filtered and interpolated melbank data
         # Grab the filtered melbank
         y = self.melbank(filtered=False, size=self.pixel_count)
-        self.out[:, 0] = self.melbank(filtered=True, size=self.pixel_count)
-        self.out[:, 1] = np.abs(y - self._prev_y)
-        self.out[:, 2] = self._b_filter.update(y)
+        self.out[:, self.rgb_mix[0]] = self.melbank(filtered=True, size=self.pixel_count)
+        self.out[:, self.rgb_mix[1]] = np.abs(y - self._prev_y)
+        self.out[:, self.rgb_mix[2]] = self._b_filter.update(y)
         self.out *= 1000
 
         self._prev_y = y
