4dca4de1e1af07beab3769a899ae9ba2580cec24
Yeon Vinzenz Varapragasam <YeonVinzenz.Varapragasam@trustedshops.de>
2023-11-17 17:02:56 +0100

    First try

---
+++ ledfx/api/get_nanoleaf_token.py
@@ -0,0 +1,34 @@
+import logging
+from json import JSONDecodeError
+
+from aiohttp import web
+
+from ledfx.api import RestEndpoint
+from ledfx.utils import async_fire_and_forget, set_name_to_icon
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class GetNanoleadTokenEndpoint(RestEndpoint):
+    """REST end-point for detecting and adding wled devices"""
+
+    ENDPOINT_PATH = "/api/getNanoleafToken"
+
+    async def post(self, request) -> web.Response:
+        """Find and add all WLED devices on the LAN"""
+        try:
+            data = await request.json()
+        except JSONDecodeError:
+            response = {
+                "status": "failed",
+                "reason": "JSON Decoding failed",
+            }
+            return web.json_response(data=response, status=400)
+
+        ip = data.get("ip_address")
+        # TODO: Get Nanoleaf Token from IP
+        _LOGGER.info(f"Get Nanoleaf Token from {ip}")
+
+        # TODO: Replace with actual token
+        response = {"status": "success", "auth_token": "1337Blade"}
+        return web.json_response(data=response, status=200)
