12e1667e708eaa52fb29b1c04c742a3f1bbaf37b
bigredfrog <anthony.tod@gmail.com>
2023-12-02 22:34:59 -0500

    enable sentry, traces onoy. requires key injection at deploy

--- ledfx/sentry_config.py
+++ ledfx/sentry_config.py
@@ -9,11 +9,11 @@ from ledfx.consts import DEV, PROJECT_VERSION
 sentry_dsn = "DSN"
 if (
     sentry_dsn
-    == "https://691086dc41fa4218860be6ed4c888145@o482797.ingest.sentry.io/5533553"
+    == "https://4c17033f52d44c3aa372c217395dc95b@o482797.ingest.sentry.io/5596233"
 ):
     sentry_sdk.init(
         sentry_dsn,
-        traces_sample_rate=1,
+        traces_sample_rate=0,
         integrations=[AioHttpIntegration()],
         release=f"ledfx@{PROJECT_VERSION}",
     )
