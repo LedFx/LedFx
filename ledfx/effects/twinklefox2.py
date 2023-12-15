e23aae2a2e92803c4b6b8d1b7779c7e2c7bdc997
Andy Dodd <atd7@cornell.edu>
2023-12-15 08:42:16 -0500

    Make frequency range selectable
    Lows works great for trance, not so great for classical Christmas music, mids seem to work best for that

--- ledfx/effects/twinklefox2.py
+++ ledfx/effects/twinklefox2.py
@@ -11,6 +11,14 @@ class Twinklefox2(AudioReactiveEffect, HSVEffect):
     NAME = "Twinklefox v2"
     CATEGORY = "Atmospheric"
 
+    _power_funcs = {
+        "Beat": "beat_power",
+        "Bass": "bass_power",
+        "Lows (beat+bass)": "lows_power",
+        "Mids": "mids_power",
+        "High": "high_power",
+    }
+
     CONFIG_SCHEMA = vol.Schema(
         {
             vol.Optional(
@@ -28,6 +36,11 @@ class Twinklefox2(AudioReactiveEffect, HSVEffect):
                 description="Twinkle density",
                 default=0.30,
             ): vol.All(vol.Coerce(float), vol.Range(min=0.00001, max=1.0)),
+            vol.Optional(
+                "frequency_range",
+                description="Frequency range for the beat detection",
+                default="Lows (beat+bass)",
+            ): vol.In(list(_power_funcs.keys())),
             vol.Optional(
                 "speed_reac",
                 description="Audio Reactivity (speed)",
@@ -51,17 +64,18 @@ class Twinklefox2(AudioReactiveEffect, HSVEffect):
         self.hue = np.random.rand(self.pixel_count)
 
     def config_updated(self, config):
-        self._lows_power = 0
-        self._lows_filter = self.create_filter(
+        self._power = 0
+        self._power_filter = self.create_filter(
             alpha_decay=0.05, alpha_rise=0.2
         )
 
         self.clk_div = np.power(2, 7 - 6*self._config["speed"])
         self.trig_thresh = (1.0/self.config["density"]) - 1.0
+        self.power_func = self._power_funcs[self._config["frequency_range"]]
 
     def audio_data_updated(self, data):
-        self._lows_power = self._lows_filter.update(
-            data.lows_power(filtered=False)
+        self._power = self._power_filter.update(
+            getattr(data, self.power_func)()
         )
 
     def array_sawtooth(self, a):
@@ -72,7 +86,7 @@ class Twinklefox2(AudioReactiveEffect, HSVEffect):
         now_ns = time.time_ns()
         dt = (now_ns - self.last_time)/1.0e6 #Original twinklefox ticks in milliseconds
 
-        dt *= 1.0 + self._lows_power*np.power(2,self._config["speed_reac"]*7-1)
+        dt *= 1.0 + self._power*np.power(2,self._config["speed_reac"]*7-1)
         #Rewrite twinklefox's clock increase in a different manner.
         self.phase += self.speed_modifier*dt/(self.clk_div*256.0)
         self.last_time = now_ns
@@ -84,7 +98,7 @@ class Twinklefox2(AudioReactiveEffect, HSVEffect):
         #Determine whether to trigger a new twinkle, and if so, choose a new random hue and speed mult, and reset phase to zero
         #Unlike original twinklefox, the deadtime for any pixel is exactly (1-density) - we rely on the speed multiplier to hide that
         #from the viewer
-        trig_idxs = (self.phase > 1.0 + (1.0-self._config["dens_reac"]*self._lows_power)*self.trig_thresh).nonzero()[0] #Can lows_power ever be > 1.0?  I don't think so?
+        trig_idxs = (self.phase > 1.0 + (1.0-self._config["dens_reac"]*self._power)*self.trig_thresh).nonzero()[0] #Can lows_power ever be > 1.0?  I don't think so?
         numup = len(trig_idxs)
 
         if(numup > 0):
