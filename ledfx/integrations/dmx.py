31b30722eeee6114bb4f271bdb85bcb844b23fad
Matt Muller <matt4788667@hotmail.com>
2023-12-17 14:47:28 +1100

    Update dmx.py

--- ledfx/integrations/dmx.py
+++ ledfx/integrations/dmx.py
@@ -1,58 +1,24 @@
 # pip install pyusb
-
 import usb.core
 import usb.util
 import asyncio
 import logging
 import aiohttp
 import voluptuous as vol
-
 from ledfx.integrations import Integration
 from ledfx.utils import async_fire_and_forget, resolve_destination
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def on_start(self):
-    # Scan and populate USB DMX devices
-    usb_devices = await self.scan_usb_dmx_devices()
-    device_names = [
-        f"{dev.idVendor:04X}:{dev.idProduct:04X}" for dev in usb_devices]
-
-    # Add USB DMX devices to dropdown field
-    self.config_schema = self.config_schema.extend(
-        {
-            vol.Required(
-                "usb_device",
-                description="Select the USB DMX device",
-                default=None,
-                valid_values=device_names,
-            ): str,
-        }
-    )
-
-
-async def scan_usb_dmx_devices(self):
-    """Scan for USB DMX devices"""
-    usb_devices = usb.core
-    # usb_devices = usb.core.find(find_all=True, idVendor=0xXXXX, idProduct=0xXXXX)  # Replace with your vendor and product IDs
-    return usb_devices
-
-
 class DMX(Integration):
     """DMX Integration"""
+
     beta = False
     _widget_types = ["Button", "Slider", "Audio Triggers"]
     NAME = "DMX USB Controller"
     DESCRIPTION = "DMX USB, For stage lighting such as DMXKing"
-
-    _usb_dmx = {
-        "Beat": "beat_power",
-        "Bass": "bass_power",
-        "Lows (beat+bass)": "lows_power",
-        "Mids": "mids_power",
-        "High": "high_power",
-    }
+    usb_devices = usb.core.find(find_all=True)  # Retrieve USB DMX devices
 
     CONFIG_SCHEMA = vol.Schema(
         {
@@ -65,21 +31,6 @@ class DMX(Integration):
                 "usb_device",
                 description="Select the USB DMX device",
                 default=None,
-            ): vol.In(list(_usb_dmx.keys())),
-            # vol.Required(
-            #     "description",
-            #     description="Description of this integration",
-            #     default="DMX USB",
-            # ): str,
+            ): vol.In([f"{dev.idVendor:04X}:{dev.idProduct:04X}" for dev in usb_devices]),
         }
     )
-
-    # Add other methods as needed
-
-
-# Example usage:
-# Create an instance of the DMX class
-# dmx_instance = DMX()
-
-# Start the integration
-# asyncio.run(dmx_instance.on_start())
