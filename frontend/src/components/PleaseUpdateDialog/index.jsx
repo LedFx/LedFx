4dd464e7790f06c336b37679e13d8faa1d8f07f0
Shaun Eccles-Smith <shauneccles@gmail.com>
2023-12-23 11:59:48 +1100

    Add rough Please Update Dialog component

---
+++ frontend/src/components/PleaseUpdateDialog/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@material-ui/core';
+
+const PleaseUpdateDialog = ({ open, onClose }) => {
+    return (
+        <Dialog open={open} onClose={onClose}>
+            <DialogTitle>Please Update Manually</DialogTitle>
+            <DialogContent>
+                <p>You are operating on a very old version of LedFx that we cannot automatically update anymore.</p>
+                <p>Please back up your configuration (on Windows it is located at %appdata%\.ledfx) and uninstall the current version of LedFx. </p>
+                <p>Once this is done you can <a href="https://download.ledfx.app">download the new version.</a></p>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={onClose} color="primary">
+                    OK
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default PleaseUpdateDialog;
