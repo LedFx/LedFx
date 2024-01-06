bb71b673bce0e60f87da2876b3db554927885e69
atod <anthony.tod@gmail.com>
2024-01-06 18:35:37 -0500

    refactor, add spin

--- ledfx/effects/equalizer2d.py
+++ ledfx/effects/equalizer2d.py
@@ -9,7 +9,6 @@ from ledfx.effects.twod import Twod
 _LOGGER = logging.getLogger(__name__)
 
 def interpolate_point(p1, p2, t):
-    """Interpolate between two points represented as tuples."""
     return (p1[0] + (p2[0] - p1[0]) * t, p1[1] + (p2[1] - p1[1]) * t)
 
 class Equalizer2d(Twod, GradientEffect):
@@ -55,6 +54,11 @@ class Equalizer2d(Twod, GradientEffect):
                 default=False,
             ): bool,
             vol.Optional(
+                "spin",
+                description="Weeeeeeeeeee",
+                default=False,
+            ): bool,
+            vol.Optional(
                 "bands",
                 description="Number of freq bands",
                 default=16,
@@ -78,6 +82,29 @@ class Equalizer2d(Twod, GradientEffect):
         self.peak_per = self._config["peak percent"]
         self.peak_decay = self.config["peak decay"]
         self.ring = self._config["ring"]
+        self.spin = self._config["spin"]
+
+    def calc_ring_segments(self, rotation):
+        # we want coordinates for self.bands around an oval defined by self.r_width and self.r_height
+        # do 1 more than bands to get closure of ring
+
+        # calculate start and end of bands on circle
+        self.bandsc = []
+        for i in range(self.bands + 1):
+            angle = 2.0 * np.pi * i / self.bands + np.radians(rotation)
+            x = (self.r_width) / 2.0 + (self.r_width - 1) / 2.0 * np.cos(angle)
+            y = (self.r_height) / 2.0 + (self.r_height - 1) / 2.0 * np.sin(
+                angle)
+            self.bandsc.append((x, y))
+
+        # calc mid points of bands
+        self.bandscm = []
+        for i in range(self.bands):
+            self.bandscm.append(
+                interpolate_point(self.bandsc[i], self.bandsc[i + 1], 0.5))
+        # Add from beginnning to end for wrap around
+        self.bandscm.append(self.bandscm[0])
+        self.p_center = (self.r_width / 2.0, self.r_height / 2.0)
 
     def do_once(self):
         super().do_once()
@@ -95,123 +122,130 @@ class Equalizer2d(Twod, GradientEffect):
         )
         self.peak_size = int(self.peak_per * self.r_height / 100)
         self.half_height = self.r_height // 2
+
         if self.ring:
-            # we want coordinates for self.bands around an oval defined by self.r_width and self.r_height
-            # more 1 than bands to get closure of ring
-            self.bandsc = []
-            for i in range(self.bands + 1):
-                angle = 2 * np.pi * i / self.bands
-                x = (self.r_width - 1) / 2 + (self.r_width-1) / 2 * np.cos(angle)
-                y = (self.r_height - 1) / 2 + (self.r_height-1) / 2 * np.sin(angle)
-                self.bandsc.append((x, y))
-            self.p_center = (self.r_width / 2, self.r_height / 2)
+            self.calc_ring_segments(0)
+            self.spin_value = 0.0
 
     def audio_data_updated(self, data):
         # Grab the filtered melbank
         self.r = self.melbank(filtered=True, size=self.pixel_count)
         np.clip(self.r, 0, 1, out=self.r)
 
-    def draw(self):
-        if self.test:
-            self.draw_test(self.m_draw)
-
+    def prep_frame_vars(self):
+        # prepare volumes, peaks and colors for drawing
+        # these are per render frame or audio frame vars
+        # so cannot be done in do_once()
         r_split = np.array_split(self.r, self.bands)
         if self.max:
-            volumes = np.array([split.max() for split in r_split])
+            self.volumes = np.array([split.max() for split in r_split])
         else:
-            volumes = np.array([split.mean() for split in r_split])
+            self.volumes = np.array([split.mean() for split in r_split])
+        self.volumes = np.append(self.volumes, self.volumes[0])
 
         if self.peak:
-            peaks = self.peaks_filter.update(volumes)
+            self.peaks = self.peaks_filter.update(self.volumes)
 
-        # Precompute values that are constant for each iteration
-        gradient_colors = [
+        self.gradient_colors = [
             tuple(self.get_gradient_color(1 / self.bands * i).astype(int))
             for i in range(self.bands)
         ]
 
+        if self.ring and self.spin:
+            # TODO: This needs to be a filtered impulse from the bass input
+            self.spin_value += 0.1
+            self.spin_value %= 360
+            self.calc_ring_segments(self.spin_value)
+
+    def draw_ring(self):
+        for i in range(self.bands):
+            if self.center:
+                self.m_draw.polygon([interpolate_point(self.p_center, self.bandsc[i], self.volumes[i]),
+                                     interpolate_point(self.p_center, self.bandsc[i + 1], self.volumes[i]),
+                                     self.p_center],
+                                    fill=self.gradient_colors[i])
+
+                if self.peak:
+                    self.m_draw.line([interpolate_point(self.p_center, self.bandscm[i], self.peaks[i]),
+                                      interpolate_point(self.p_center, self.bandscm[i + 1], self.peaks[i + 1])],
+                                     fill=(255, 255, 255),
+                                     width=self.peak_size)
+            else:
+                self.m_draw.polygon([self.bandsc[i],
+                                     self.bandsc[i + 1],
+                                     interpolate_point(self.bandscm[i], self.p_center, self.volumes[i])],
+                                    fill=self.gradient_colors[i],
+                                    outline = tuple(max(component - 1, 0) for component in self.gradient_colors[i]))
+
+                if self.peak:
+                    self.m_draw.line([interpolate_point(self.bandscm[i], self.p_center, self.peaks[i]),
+                                      interpolate_point(self.bandscm[i + 1], self.p_center, self.peaks[i + 1])],
+                                     fill=(255, 255, 255),
+                                     width=self.peak_size)
+
+    def draw_normal(self):
+        for i in range(self.bands):
+            band_start, band_end = self.bandsx[i]
+            volume_scaled = int(self.r_height * self.volumes[i])
+            if self.center:
+                # Calculate dimensions for the centered rectangle
+                bottom = self.half_height - volume_scaled // 2
+                top = self.half_height + volume_scaled // 2
+            else:
+                # Dimensions for the bottom to top rectangle
+                bottom = 0
+                top = volume_scaled
+
+            # Draw the rectangle
+            self.m_draw.rectangle(
+                (band_start, bottom, band_end, top),
+                fill=self.gradient_colors[i]
+            )
+
+            # Draw the peak marker
+            if self.peak:
+                if self.center:
+                    peak_scaled = int(self.half_height * self.peaks[i])
+                    peak_end = int(peak_scaled + self.peak_size // 2)
+                    self.m_draw.rectangle(
+                        (
+                            band_start,
+                            self.half_height + peak_scaled,
+                            band_end,
+                            self.half_height + peak_end,
+                        ),
+                        fill=(255, 255, 255),
+                    )
+                    self.m_draw.rectangle(
+                        (
+                            band_start,
+                            self.half_height - peak_end,
+                            band_end,
+                            self.half_height - peak_scaled,
+                        ),
+                        fill=(255, 255, 255),
+                    )
+                else:
+                    peak_scaled = int(self.r_height * self.peaks[i])
+                    peak_end = peak_scaled + self.peak_size
+
+                    self.m_draw.rectangle(
+                        (band_start, peak_scaled, band_end, peak_end),
+                        fill=(255, 255, 255),
+                    )
+
+    def draw(self):
+        # note we are leaving all math in float space until it gets clipped
+        # down to int by Image draw functions
+
+        if self.test:
+            self.draw_test(self.m_draw)
+
+        self.prep_frame_vars()
+
         if self.ring:
-            for i in range(self.bands):
-                # lets start by plotting a pixel at each coordinate in self.bandsx
-                #self.m_draw.point(self.bandsc[i], fill=gradient_colors[i])
-
-                # _LOGGER.info(f"i: {i} bandsc: {self.bandsc[i]} bandsc+1: {self.bandsc[i+1]} center: {self.center}")
-                # draw a triangle from the to the value pairs in self.bandsc
-
-                # self.m_draw.point(reach, (255,255,255))
-
-                if self.peak:
-                    if i == self.bands - 1:
-                        peak_end = peaks[0]
-                    else:
-                        peak_end = peaks[i + 1]
-
-                if self.center:
-                    p1 = interpolate_point(self.p_center, self.bandsc[i], volumes[i])
-                    p2 = interpolate_point(self.p_center, self.bandsc[i+1], volumes[i])
-                    self.m_draw.polygon([p1, p2, self.p_center], fill=gradient_colors[i])
-
-                    if self.peak:
-                        p1 = interpolate_point(self.p_center, self.bandsc[i], peaks[i])
-                        p2 = interpolate_point(self.p_center, self.bandsc[i+1], peak_end)
-                        self.m_draw.line([p1, p2], fill=(255,255,255), width=self.peak_size)
-                else:
-                    base = interpolate_point(self.bandsc[i], self.bandsc[i + 1], 0.5)
-                    reach = interpolate_point(base, self.p_center, volumes[i])
-                    self.m_draw.polygon([self.bandsc[i], self.bandsc[i+1], reach], fill=gradient_colors[i])
-
-                    if self.peak:
-                        p1 = interpolate_point(self.bandsc[i], self.p_center, peaks[i])
-                        p2 = interpolate_point(self.bandsc[i+1], self.p_center, peak_end)
-                        self.m_draw.line([p1, p2], fill=(255,255,255), width=self.peak_size)
+            self.draw_ring()
         else:
-            for i in range(self.bands):
-                band_start, band_end = self.bandsx[i]
-                volume_scaled = int(self.r_height * volumes[i])
-                if self.center:
-                    # Calculate dimensions for the centered rectangle
-                    bottom = self.half_height - volume_scaled // 2
-                    top = self.half_height + volume_scaled // 2
-                else:
-                    # Dimensions for the bottom to top rectangle
-                    bottom = 0
-                    top = volume_scaled
-
-                # Draw the rectangle
-                self.m_draw.rectangle(
-                    (band_start, bottom, band_end, top), fill=gradient_colors[i]
-                )
-
-                # Draw the peak marker
-                if self.peak:
-                    if self.center:
-                        peak_scaled = int(self.half_height * peaks[i])
-                        peak_end = int(peak_scaled + self.peak_size // 2)
-                        self.m_draw.rectangle(
-                            (
-                                band_start,
-                                self.half_height + peak_scaled,
-                                band_end,
-                                self.half_height + peak_end,
-                            ),
-                            fill=(255, 255, 255),
-                        )
-                        self.m_draw.rectangle(
-                            (
-                                band_start,
-                                self.half_height - peak_end,
-                                band_end,
-                                self.half_height - peak_scaled,
-                            ),
-                            fill=(255, 255, 255),
-                        )
-                    else:
-                        peak_scaled = int(self.r_height * peaks[i])
-                        peak_end = peak_scaled + self.peak_size
-
-                        self.m_draw.rectangle(
-                            (band_start, peak_scaled, band_end, peak_end),
-                            fill=(255, 255, 255),
-                        )
+            self.draw_normal()
 
         self.roll_gradient()
