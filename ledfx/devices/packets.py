f1a5ffac22ed01ee794a3a02ea7128b98e151c92
Mario Guggenberger <mg@protyposis.net>
2024-02-04 02:53:28 +0100

    feat: HyperHDR support (#698)
    * feat: HyperHDR support
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- ledfx/devices/packets.py
+++ ledfx/devices/packets.py
@@ -59,6 +59,23 @@ def build_drgb_packet(data: np.ndarray, timeout: int):
     return packet
 
 
+def build_rgb_packet(data: np.ndarray):
+    """
+    Generic RGB packet encoding (used by HyperHDR's "UDP raw receiver")
+    Max LEDs: 500
+
+    Header: none
+    Byte 	Description
+    0 + n*3 	Red Value
+    1 + n*3 	Green Value
+    2 + n*3 	Blue Value
+
+    """
+    byteData = data.astype(np.dtype("B"))
+    packet = byteData.flatten().tobytes()
+    return packet
+
+
 def build_drgbw_packet(data: np.ndarray, timeout: int):
     """
     Generic DRGBW packet encoding
