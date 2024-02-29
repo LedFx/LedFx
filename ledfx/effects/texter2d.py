8e378d2f50c5e0a031ccbcf4089d2982416d6ed2
atod <anthony.tod@gmail.com>
2024-02-29 18:51:27 -0500

    words are rendering

--- ledfx/effects/texter2d.py
+++ ledfx/effects/texter2d.py
@@ -12,14 +12,7 @@ from PIL import Image, ImageDraw, ImageFont
 
 _LOGGER = logging.getLogger(__name__)
 
-# copy this file and rename it into the effects folder
-# Anywhere you see template, replace it with your own class reference / name
-
-# create a dict of font name strings and file locations for opening from pillow
-
-# Replace Robot Crush
-# Find better fonts
-# Work out why value validation is going pop
+DEBUG = False
 
 FONT_MAPPINGS = {
     "Roboto Regular": os.path.join(LEDFX_ASSETS_PATH, "fonts", "Roboto-Regular.ttf"),
@@ -38,7 +31,7 @@ class Textblock():
     # the Textblock instance will be merged into the main display image outside of this class
     # so TextBlock has no idea of position which will be handled externally to this class
 
-    def __init__(self, text, font, color):
+    def __init__(self, text, font, color='white'):
         self.text = text
         # open the font file
         self.color = color
@@ -48,11 +41,71 @@ class Textblock():
         left, top, right, bottom = dummy_draw.textbbox((0,0), text, font=font)
         self.width = right - left
         self.height = self.descent + self.ascent
-        self.image = Image.new('RGBA', (self.width, self.height), "blue")
+        self.image = Image.new('RGBA', (self.width, self.height))
         self.draw = ImageDraw.Draw(self.image)
         self.draw.text((0, 0), self.text, font=font, fill=color)
         # self.image.show()
 
+    def set_pos_life(self, pos, vel, ang, size, life):
+        self.pos = pos
+        self.vel = vel
+        self.ang = ang
+        self.size = size
+        self.life = life
+
+    def render(self, target):
+        # first we will rotate and then size the image
+        # ang is a rotation from 0 to 1 float to represent 0 to 360 degrees
+        # size is a float from 0 to 1 to represent 0 to 100% size
+        resized = self.image.rotate(self.ang * 360, expand=True)
+        resized = resized.resize((int(resized.width * self.size), int(resized.height * self.size)))
+        # self.pos is a scalar for x and y in the range -1 to 1
+        # the pos position is for the center of the image
+        # here we will convert it to a pixel position within target which is a PIL image object
+        pos_xy = (int((self.pos[0] + 1) * target.width / 2 - resized.width / 2),
+                  int((self.pos[1] + 1) * target.height / 2 - resized.height / 2))
+
+        target.paste(resized, pos_xy, resized)
+
+
+class Sentence():
+    # this class will construct and maintain a set of words,
+    # spaces and animated dynamics for a sentence
+
+    ANIMATIONS = {
+        "SIDE_SCROLL"
+    }
+
+    def __init__(self, text, font_name, points, start_color='white'):
+        self.text = text
+        self.font_path = FONT_MAPPINGS[font_name]
+        self.points = points
+        self.start_color = parse_color(start_color)
+        self.font = ImageFont.truetype(self.font_path, self.points)
+        self.wordblocks = []
+
+        for word in self.text.split():
+            wordblock = Textblock(word,
+                             self.font,
+                             self.start_color)
+            self.wordblocks.append(wordblock)
+            _LOGGER.info(f"Wordblock {word} created")
+            if DEBUG:
+                wordblock.image.show()
+        space_block = Textblock(" ",
+                                self.font)
+        self.space_width = space_block.width
+        _LOGGER.info(f"Space width is {self.space_width}")
+
+
+        for idx, word in enumerate(self.wordblocks):
+            word.set_pos_life((-1 + idx * 0.2, -1 + idx * 0.2), (0, 0), 0.1 * idx, 0.2, 1)
+            _LOGGER.info(f"{idx} {word.text} {word.width} {word.height} {word.pos} {word.vel} {word.ang} {word.size} {word.life}")
+
+    def render(self, target):
+        for word in self.wordblocks:
+            if word.life > 0:
+                word.render(target)
 
 class Texter2d(Twod):
     NAME = "Texter"
@@ -104,40 +157,26 @@ class Texter2d(Twod):
         super().do_once()
         # self.r_width and self.r_height should be used for the (r)ender space
         # as the self.matrix will not exist yet
-        self.font_path = FONT_MAPPINGS[self.config["font"]]
-        self.points = int(self.r_height * self.config["height_percent"] / 100)
-        self.font = ImageFont.truetype(self.font_path, self.points)
-
-        self.wordblocks = []
-        # tokenise the text into a list of words
-        # then iterate through the list, rendering each word
-        for word in self.config["text"].split():
-            wordblock = Textblock(word,
-                             self.font,
-                             parse_color(self.config["text_color"]))
-            self.wordblocks.append(wordblock)
-            _LOGGER.info(f"Wordblock {word} created")
+        self.sentence = Sentence(self.config["text"],
+                                 self.config["font"],
+                                 int(self.r_height * self.config["height_percent"] / 100),
+                                 self.config["text_color"])
 
     def audio_data_updated(self, data):
         # Grab your audio input here, such as bar oscillator
         self.bar = data.bar_oscillator()
 
     def draw(self):
-        # this is where you pixel mash, it will be a black image object each call
-        # a draw object is already attached
         # self.matrix is the Image object
         # self.m_draw is the attached draw object
 
-        # all rotation abstraction is done for you
-        # you can use image dimensions now
         # self.matrix.height
         # self.matrix.width
 
-        # look in this function for basic lines etc, use pillow primitives
-        # for regular shapes
         if self.test:
             self.draw_test(self.m_draw)
 
+        self.sentence.render(self.matrix)
         # stuff pixels with
         # self.matrix.putpixel((x, y), (r, g, b))
         # or
