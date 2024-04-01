266b8f1bce36d4db81269051b98ced51d61d4e0d
Shaun Eccles-Smith <shauneccles@gmail.com>
2024-04-01 18:36:34 +1100

    Bump versions and update frontend assets for 2.0.95

--- ledfx/consts.py
+++ ledfx/consts.py
@@ -1,13 +1,13 @@
 import ledfx_assets
 
 # TODO: Bump pyproject.toml if you bump this!
-PROJECT_VERSION = "2.0.94"
+PROJECT_VERSION = "2.0.95"
 PROJECT_NAME = "LedFx"
 PROJECT_AUTHOR = "LedFx Developers"
 PROJECT_LICENSE = "GPL-3.0"
 CONFIG_MAJOR_VERSION = 2
 CONFIG_MINOR_VERSION = 3
-CONFIG_MICRO_VERSION = 0
+CONFIG_MICRO_VERSION = 1
 
 # Dev turns sentry logging on and off
 DEV = 0
