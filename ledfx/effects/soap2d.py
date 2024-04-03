4831bf20af33e85f0d3c59b8ef1ff9667e8d7933
atod <anthony.tod@gmail.com>
2024-04-03 17:40:03 -0400

    soap prototyping

--- ledfx/effects/soap2d.py
+++ ledfx/effects/soap2d.py
@@ -39,15 +39,10 @@ class Soap2d(Twod, GradientEffect):
     CONFIG_SCHEMA = vol.Schema(
         {
             vol.Optional(
-                "a_switch",
-                description="Does a boolean thing",
-                default=False,
-            ): bool,
-            vol.Optional(
                 "speed",
                 description="Speed of the effect",
-                default = 128,
-            ): vol.All(vol.Coerce(int), vol.Range(min=0, max=255)),
+                default = 1,
+            ): vol.All(vol.Coerce(float), vol.Range(min=0, max=5)),
             vol.Optional(
                 "intensity",
                 description="intensity of the effect",
@@ -63,6 +58,16 @@ class Soap2d(Twod, GradientEffect):
                 description="zoom density",
                 default=5,
             ): vol.All(vol.Coerce(float), vol.Range(min=0.5, max=20)),
+            vol.Optional(
+                "impulse_decay",
+                description="Decay filter applied to the impulse for development",
+                default=0.1,
+            ): vol.All(vol.Coerce(float), vol.Range(min=0.01, max=0.3)),
+            vol.Optional(
+                "multiplier",
+                description="audio injection multiplier, 0 is none",
+                default=1.0,
+            ): vol.All(vol.Coerce(float), vol.Range(min=0.0, max=4.0)),
         }
     )
 
@@ -78,6 +83,12 @@ class Soap2d(Twod, GradientEffect):
         self.intensity = self._config["intensity"]
         self.stretch = self._config["stretch"]
         self.zoom = self._config["zoom"]
+        self.multiplier = self._config["multiplier"]
+
+        self.lows_impulse_filter = self.create_filter(
+            alpha_decay=self._config["impulse_decay"], alpha_rise=0.99
+        )
+        self.lows_impulse = 0
 
     # prior to rework at 32 x 32 with int / float mix and brute force 80 ms / frame
     def do_once(self):
@@ -90,23 +101,25 @@ class Soap2d(Twod, GradientEffect):
         self.noise_z = random.random()
         self.scale_x = self.zoom/cols
         self.scale_y = self.zoom/rows
-        self.mov = min(cols, rows)*(self.speed + 2)/2/65535
+
         self.smoothness = min(250, self.intensity)
         self.seed_image = Image.new("RGB", (self.r_width, self.r_height))
         self.seed_matrix = True
         self.noise = vnoise.Noise()
 
     def audio_data_updated(self, data):
-        # Grab your audio input here, such as bar oscillator
-        self.bar = data.bar_oscillator()
+        self.lows_impulse = self.lows_impulse_filter.update(
+            data.lows_power(filtered=False) * self.multiplier
+        )
 
     def draw(self):
 
         if self.test:
             self.draw_test(self.m_draw)
 
-        # move the plane through the noise space
-        # this is a candidate for audio reaction
+        # time invariant movement throuh the noise space
+        self.mov = 0.5 * self.speed * self.passed
+
         self.noise_x += self.mov
         self.noise_y += self.mov
         self.noise_z += self.mov
@@ -122,20 +135,35 @@ class Soap2d(Twod, GradientEffect):
 
         # generate arrays of the X adn Y axis of our plane, with a singular Z
         # this should allow libs to use any internal acceleration for unrolling across all points
-
-        start = timeit.default_timer()
-        x_array = np.linspace(self.noise_x, self.noise_x + self.scale_x * cols, cols)
-        y_array = np.linspace(self.noise_y, self.noise_y + self.scale_y * rows, rows)
-        z_array = np.array([self.noise_z])
-        next1 = timeit.default_timer()
         if log:
+            start = timeit.default_timer()
+
+        bass_x = self.scale_x * self.lows_impulse
+        bass_y = self.scale_y * self.lows_impulse
+
+        scale_x = self.scale_x + bass_x
+        scale_y = self.scale_y + bass_y
+
+        noise_x = self.noise_x - ( scale_x * rows / 2)
+        noise_y = self.noise_y - ( scale_y * rows / 2)
+
+        x_array = np.linspace(noise_x, noise_x + scale_x * rows, rows)
+        y_array = np.linspace(noise_y, noise_y + scale_y * cols, cols)
+        z_array = np.array([self.noise_z])
+        if log:
+            next1 = timeit.default_timer()
             _LOGGER.info(f"array generation time: {next1 - start}")
 
+        ###################################################################################
         # This is where the magic happens, calling the lib of choice to get the noise plane
+        ###################################################################################
+# opensimplex at 128x128 on dev machine is 200 ms per frame - Unusable
 #        self.noise_3d = opensimplex.noise3array(x_array, y_array, z_array)
-        self.noise_3d = self.noise.noise3(x_array, y_array, z_array)
-        next2 = timeit.default_timer()
+# vnoise at 128x128 on dev machine is 2.5 ms per frame - Current best candidate
+        self.noise_3d = self.noise.noise3(x_array, y_array, z_array, grid_mode=True)
+
         if log:
+            next2 = timeit.default_timer()
             _LOGGER.info(f"simple noise time: {next2 - next1}")
 
         # if the lib happens to return in a 3 dimensionsal, even though Z is depth of 1, then squeeze it down
@@ -145,8 +173,9 @@ class Soap2d(Twod, GradientEffect):
         self.noise_stretched = self.noise_squeezed * self.stretch
         # normalise the noise from -1,1 range to 0,1
         self.noise_normalised = (self.noise_stretched + 1) / 2
-        next3 = timeit.default_timer()
+
         if log:
+            next3 = timeit.default_timer()
             _LOGGER.info(f"simple squeeze time: {next3 - next2}")
 
         # _LOGGER.info(f"x_array: {x_array}")
@@ -158,15 +187,17 @@ class Soap2d(Twod, GradientEffect):
 
         # map from 0,1 space into the gradient color space via our nicely vecotrised function
         self.color_array = self.get_gradient_color_vectorized2d(self.noise_normalised).astype(np.uint8)
-        next4 = timeit.default_timer()
+
         if log:
+            next4 = timeit.default_timer()
             _LOGGER.info(f"color array time: {next4 - next3}")
 
         # _LOGGER.info(f"color_array: {self.color_array}")
 
         # transform the numpy array into a PIL image in one easy step
         self.matrix = Image.fromarray(self.color_array, "RGB")
-        next5 = timeit.default_timer()
+
         if log:
+            next5 = timeit.default_timer()
             _LOGGER.info(f"image from array time: {next5 - next4}")
 
