21cf3b2192fc5b3ce3021478c9cefb9b6ab95713
atod <anthony.tod@gmail.com>
2024-04-14 16:28:39 -0400

    First add of digital rain

---
+++ ledfx/effects/digitalrain2d.py
@@ -0,0 +1,154 @@
+import logging
+
+import voluptuous as vol
+import random
+import numpy as np
+
+from ledfx.effects.twod import Twod
+from ledfx.effects.gradient import GradientEffect
+
+_LOGGER = logging.getLogger(__name__)
+
+class Line():
+    def __init__(self, nx, color, offset, speed):
+        self.nx = nx
+        self.ny = 0
+        self.color = color
+        # per line modifyer on speed, norm 1.0
+        self.speed = speed
+        self.tail = 0.5
+        self.offset = offset
+
+    def update(self, run_seconds, passed, tail):
+        # calculate how much to move
+        movement = passed / run_seconds
+        self.tail = tail
+        # adjust for the code lines own speed
+        self.ny += movement * self.speed
+        # _LOGGER.info(f"{self.color} {self.ny:.4f} {self.ny:.4f} {self.speed:.4f} {movement:.4f}")
+        if self.ny > (1 + self.tail):
+            return False
+        return True
+
+    def draw(self, draw, image, width, beat_osc):
+        x = int(self.nx * image.width)
+        y = int(self.ny * image.height)
+        line_width = max(1, int(image.width * (width / 100.0)))
+        tail_length = int(image.height * self.tail)
+        # _LOGGER.info(f"w {width} iw: {image.width} cw {int(image.width * (width / 100.0))} {line_width}")
+
+        draw.line(
+            (
+                x, y,
+                x, y - tail_length
+            ),
+            width=line_width,
+            fill=tuple(self.color.astype(int))
+        )
+
+        beat_roll = beat_osc + self.offset
+        beat_roll = beat_roll - np.floor(beat_roll)
+
+        draw.line(
+            (
+                x, y,
+                x, y - (line_width - 1)
+            ),
+            width=line_width,
+            fill=tuple((np.array([255, 255, 255]) * (0.5 + beat_roll * 0.5)).astype(int))
+        )
+
+class Matrix2d(Twod, GradientEffect):
+    NAME = "Digital Rain"
+    CATEGORY = "Matrix"
+    # add keys you want hidden or in advanced here
+    HIDDEN_KEYS = Twod.HIDDEN_KEYS + []
+    ADVANCED_KEYS = Twod.ADVANCED_KEYS + []
+
+    CONFIG_SCHEMA = vol.Schema(
+        {
+            vol.Optional(
+                "count",
+                description="Number of code lines in the matrix as a multiplier of matrix pixel width",
+                default=1.0,
+            ): vol.All(vol.Coerce(float), vol.Range(min=0.01, max=4.0)),
+            vol.Optional(
+                "add_speed",
+                description="Number of code lines to add per second",
+                default=5.0,
+            ): vol.All(vol.Coerce(float), vol.Range(min=0.1, max=30.0)),
+            vol.Optional(
+                "a_switch",
+                description="Does a boolean thing",
+                default=False,
+            ): bool,
+            vol.Optional(
+                "width",
+                description="Width of code lines as % of matrix",
+                default=1,
+            ): vol.All(vol.Coerce(int), vol.Range(min=1, max=30)),
+            vol.Optional(
+                "run_seconds",
+                description="Minimum number of seconds for a code line to run from top to bottom",
+                default=5.0,
+            ): vol.All(vol.Coerce(float), vol.Range(min=1, max=10.0)),
+            vol.Optional(
+                "tail",
+                description="Code line tail length as a % of the matrix",
+                default = 50,
+            ): vol.All(vol.Coerce(int), vol.Range(min=1, max=100)),
+        }
+    )
+
+    def __init__(self, ledfx, config):
+        super().__init__(ledfx, config)
+        self.bar = 0
+        self.lines = []
+        self.beat_osc = 0.0
+
+    def config_updated(self, config):
+        super().config_updated(config)
+        # copy over your configs here into variables
+        self.a_switch = self._config["a_switch"]
+        self.add_speed = self._config["add_speed"]
+        self.last_added = 0.0
+        self.width = self._config["width"]
+        self.run_seconds = self._config["run_seconds"]
+        self.tail = self._config["tail"] / 100.0
+
+    def do_once(self):
+        super().do_once()
+        self.count = max(1, int(self._config["count"] * self.r_width))
+
+    def audio_data_updated(self, data):
+        # Grab your audio input here, such as bar oscillator
+        self.beat_osc = data.beat_oscillator()
+
+    def add_line(self):
+        # let aging deal with how many lines
+        # maybe revisit later
+        # _LOGGER.info(f"current: {len(self.lines)} count: {self.count}")
+        if len(self.lines) < self.count:
+            color = self.get_gradient_color(random.random())
+            self.lines.append(Line(random.random(),
+                                   color,
+                                   random.random(),
+                                   0.1 + random.random() * 0.9))
+
+    def draw(self):
+
+        if self.test:
+            self.draw_test(self.m_draw)
+
+        # _LOGGER.info(f"FRAME")
+        self.last_added += self.passed
+        while self.last_added >= 1.0 / self.add_speed:
+            self.last_added -= 1.0 / self.add_speed
+            self.add_line()
+
+        # olderst lines are first which suits z order
+        for line in self.lines[:]:
+            if not line.update(self.run_seconds, self.passed, self.tail):
+                self.lines.remove(line)
+            line.draw(self.m_draw, self.matrix, self.width, self.beat_osc)
+
