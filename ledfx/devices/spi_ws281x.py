f8cfb1e43c185ad017b03df19d6aa1cc057566c8
bbbergh <80261502+bbbergh@users.noreply.github.com>
2024-04-23 14:49:49 +0100

    Added SPI WS281X device (for RGB and RGBW strips)

---
+++ ledfx/devices/spi_ws281x.py
@@ -0,0 +1,93 @@
+import itertools
+import logging
+
+import numpy as np
+import voluptuous as vol
+
+from ledfx.devices import Device
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class SPI_WS281X(Device):
+    """
+    RPi WS281X device support via SPI (works with RPi 5). Based on https://github.com/fschrempf/py-neopixel-spidev/
+    """
+
+    @staticmethod
+    @property
+    def CONFIG_SCHEMA():
+        return vol.Schema(
+            {
+                vol.Required(
+                    "pixel_count",
+                    description="Number of individual pixels",
+                    default=1,
+                ): vol.All(int, vol.Range(min=1)),
+                vol.Required(
+                    "spi_bus",
+                    description="Raspberry Pi SPI Bus LEDs are connected to (using SPI Bus 1 on a Pi requires special setup).",
+                    default=0,
+                ): vol.In([0, 1]),
+                vol.Required(
+                    "color_order", description="Color order", default="RGB"
+                ): vol.In(list("".join(p) for p in itertools.permutations("RGB"))),
+            }
+        )
+
+    def __init__(self, ledfx, config):
+        super().__init__(ledfx, config)
+        self.LED_FREQ_HZ = 800000
+        self.SPI_DEVICE = 0
+
+    def activate(self):
+        try:
+            import spidev
+        except ImportError as e:
+            described = ImportError("Unable to load spidev module. If you are on a raspberry pi, or another SPI capable linux device, "
+                                    "you can install spidev with 'python -m pip install spidev'.")
+            _LOGGER.error(described)
+            self.deactivate()
+            raise described from e
+
+        self.spi = spidev.SpiDev()
+        try:
+            self.spi.open(self._config["spi_bus"], self.SPI_DEVICE)
+        except OSError as e:
+            described = OSError("Unable to open SPI device. If you are on a raspberry pi, "
+                                "you may need to enable SPI in 'sudo raspi-config'. Otherwise make sure the device is not used by another process.")
+            _LOGGER.error(described)
+            self.deactivate()
+            raise described from e
+        self.spi.mode = 0
+        self.spi.max_speed_hz = int(4 * self.LED_FREQ_HZ)  # 4 SPI bits for one period
+        super().activate()
+
+    def deactivate(self):
+        if hasattr(self, "spi"):
+            self.spi.close()
+        super().deactivate()
+
+    def flush(self, data):
+        """Flush LED data to the strip"""
+        byteData = data.astype(np.dtype("B"))
+        self.spi_write(self.get_ordered_pixel_data(byteData))
+
+    def get_ordered_pixel_data(self, data):
+        return data[:, self.get_rgb_indices()]
+
+    def spi_write(self, data):
+        """
+        Write data to the SPI bus in the WS281x format
+        """
+
+        d = np.asarray(data).ravel()
+        tx = np.zeros(len(d) * 4, dtype=np.uint8)
+        for ibit in range(4):
+            tx[3 - ibit::4] = ((d >> (2 * ibit + 1)) & 1) * 0x60 + \
+                              ((d >> (2 * ibit + 0)) & 1) * 0x06 + 0x88
+        self.spi.writebytes2(tx)  # This doesn't convert to a python list, so it's faster than xfer or writebytes
+
+    def get_rgb_indices(self):
+        color_order = self._config["color_order"]
+        return ["RGB".index(c) for c in color_order]
---
+++ ledfx/devices/spi_ws281x_rgbw.py
@@ -0,0 +1,50 @@
+import itertools
+from enum import Enum
+
+import numpy as np
+import voluptuous as vol
+
+from ledfx.devices.spi_ws281x import SPI_WS281X
+
+
+class WhiteMode(Enum):
+    Replace = "Replace"
+    Add = "Add"
+
+
+class SPI_WS281X_RBGW(SPI_WS281X):
+    @staticmethod
+    @property
+    def CONFIG_SCHEMA():
+        return vol.Schema(
+            {
+                vol.Required(
+                    "white_mode",
+                    description="Whether the white channel should replace the RGB channels' white or should be added to it. "
+                                "After the white value is extracted from the RGB values, in 'Replace' mode "
+                                "the RGB channels' values are reduced by the white amount. In 'Add' mode the RGB values stay the same and "
+                                "the the RGB channels will display the white on top of the White LED. "
+                                "If you select 'Add', please make sure your setup is designed to handle the power consumption and heat dissipation of"
+                                "all four RGBW channels at maximum brightness.",
+                    default="Replace",
+                ): vol.In(list(e.value for e in WhiteMode)),
+                vol.Required(
+                    "color_order", description="Color order", default="RGBW"
+                ): vol.In("".join(p) for p in list(itertools.permutations("RGBW"))),
+            }
+        )
+
+    def get_ordered_pixel_data(self, data):
+        rgb = data
+
+        # Calculate the white value as the minimum of the RGB values, and return it as a [N, 1] array (i.e. a column vector)
+        w = np.min(rgb, axis=1)[:, np.newaxis]
+        if self._config["white_mode"] == WhiteMode.Replace.value:
+            rgb = rgb - w  # Subtract the white value from the RGB values
+
+        # Stack the RGB and W values together, and reorder them according to the color order
+        return np.concatenate((rgb, w), axis=1)[:, self.get_rgbw_indices()]
+
+    def get_rgbw_indices(self):
+        color_order = self._config["color_order"]
+        return ["RGBW".index(c) for c in color_order]
