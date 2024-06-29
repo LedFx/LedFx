814a2ca81a99e4e603b3326a7f0a8d341e22f542
atod <anthony.tod@gmail.com>
2024-06-28 21:57:35 -0400

    Dummy device for dev and users to experiment without spamming the network interface

---
+++ ledfx/devices/dummy.py
@@ -0,0 +1,38 @@
+import logging
+
+from ledfx.devices import Device
+import voluptuous as vol
+from numpy import ndarray
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class DummyDevice(Device):
+    """Dummy device for brower render only"""
+
+    CONFIG_SCHEMA = vol.Schema(
+        {
+            vol.Required(
+                "pixel_count",
+                description="Number of individual pixels",
+                default=1,
+            ): vol.All(int, vol.Range(min=1)),
+        }
+    )
+
+    def __init__(self, ledfx, config):
+        super().__init__(ledfx, config)
+        self._device_type = "Dummy"
+
+    def flush(self, data: ndarray) -> None:
+        """
+        Throw pixels into the abyss
+
+        Args:
+            data (ndarray): The LED data to be flushed.
+
+        Raises:
+            AttributeError: If an attribute error occurs during the flush.
+            OSError: If an OS error occurs during the flush.
+        """
+        pass
