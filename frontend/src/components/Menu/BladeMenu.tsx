e563a4fe40625c5a530d71e3e01bbda632eedbdf
YeonV <trusted.yeon@gmail.com>
2024-10-04 18:08:54 +0200

    remove wrong frontend src

--- frontend/src/components/Menu/BladeMenu.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Menu from '@mui/material/Menu'
-
-const BladeMenu = React.forwardRef((props, ref) => {
-  return <Menu ref={ref as any} open={false} {...props} />
-})
-
-export default BladeMenu
