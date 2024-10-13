48b72d251bbc505136d1492b3585684653b5d792
atod <anthony.tod@gmail.com>
2024-10-13 14:42:53 -0400

    fixes

--- ledfx/transitions.py
+++ ledfx/transitions.py
@@ -33,7 +33,7 @@ class Transitions(metaclass=IterClass):
         assert 0 <= weight <= 1
 
     def pre_validate(self, x1, x2):
-        if np.shape(x1) != () and np.shape(x2) != () and np.shape(x1) != np.shape(x2):
+        if np.shape(x1) != np.shape(x2):
             _LOGGER.error(f"Transitions.pre_validate: Shapes of x1 and x2 do not match: {np.shape(x1)} != {np.shape(x2)}")
             return False
         return True
