5e1e87125d2acbb91f0f7148ed9d7ccadaca2f57
atod <anthony.tod@gmail.com>
2024-10-17 17:52:08 -0400

    getting reading for stretch

--- ledfx/effects/blender.py
+++ ledfx/effects/blender.py
@@ -8,6 +8,27 @@ from ledfx.effects.audio import AudioReactiveEffect
 _LOGGER = logging.getLogger(__name__)
 
 
+def stretch_2d_full(source, target):
+    _LOGGER.warning("Stretch 2d full not implemented")
+
+def stretch_2d_repeat(source, target):
+    _LOGGER.warning("Stretch 1d repeat not implemented")
+
+def stretch_1d_vertical(source, target):
+    _LOGGER.warning("Stretch 1d vertical not implemented")
+
+def stretch_1d_horizontal(source, target):
+    _LOGGER.warning("Stretch 1d horizontal not implemented")
+
+
+STRETCH_FUNCS_MAPPING = {
+    "2d full": stretch_2d_full,
+    "2d repeat": stretch_2d_repeat,
+    "1d vertical": stretch_1d_vertical,
+    "1d horizontal": stretch_1d_horizontal,
+}
+
+
 class Blender(AudioReactiveEffect):
     NAME = "Blender"
     # CATEGORY defines where in the UI the effect will be displayed in the effects list
@@ -20,6 +41,21 @@ class Blender(AudioReactiveEffect):
 
     CONFIG_SCHEMA = vol.Schema(
         {
+            vol.Optional(
+                "mask_stretch",
+                description="How to stretch the mask source pixles to the effect pixels",
+                default="2d full",
+            ): vol.In(list(STRETCH_FUNCS_MAPPING.keys())),
+            vol.Optional(
+                "background_stretch",
+                description="How to stretch the mask source pixles to the effect pixels",
+                default="2d full",
+            ): vol.In(list(STRETCH_FUNCS_MAPPING.keys())),
+            vol.Optional(
+                "foreground_stretch",
+                description="How to stretch the mask source pixles to the effect pixels",
+                default="2d full",
+            ): vol.In(list(STRETCH_FUNCS_MAPPING.keys())),
             vol.Optional(
                 "mask",
                 description="The virtual from which to source the mask",
@@ -62,7 +98,10 @@ class Blender(AudioReactiveEffect):
         self.background = self._config["background"]
         self.invert_mask = self._config["invert_mask"]
         self.bias_black = self._config["bias_black"]
-        pass
+
+        self.mask_stretch_func = STRETCH_FUNCS_MAPPING[self._config["mask_stretch"]]
+        self.foreground_stretch_func = STRETCH_FUNCS_MAPPING[self._config["foreground_stretch"]]
+        self.background_stretch_func = STRETCH_FUNCS_MAPPING[self._config["background_stretch"]]
 
     def audio_data_updated(self, data):
         pass
@@ -109,10 +148,14 @@ class Blender(AudioReactiveEffect):
         if self.invert_mask:
             mask = ~mask
 
+        self.mask_stretch_func(1,2)
+        self.foreground_stretch_func(1,2)
+        self.background_stretch_func(1,2)
+
         # Initialize result with zeros (black) of the size of background_pixels
         # TODO: Move all sizing to a stretch model with user options
         # TODO: will support 1d / 2d and various directions of stretch
-        blending_pixels = np.zeros_like(background_pixels)
+        blending_pixels = np.zeros_like(self.pixels)
 
         # Apply the inverse of the mask to source2
         blending_pixels[~mask] = background_pixels[~mask]
@@ -122,3 +165,5 @@ class Blender(AudioReactiveEffect):
 
         # Assign the final result
         self.pixels = blending_pixels
+
+
