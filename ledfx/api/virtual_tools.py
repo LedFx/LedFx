ffbb9bf36f84029aec395d7eb3f399411784a091
Valentin Yuryev <goodfaiter@gmail.com>
2025-04-02 15:03:19 +0200

    Added oneshot POST for virtual_tools

--- ledfx/api/virtual_tools.py
+++ ledfx/api/virtual_tools.py
@@ -19,6 +19,53 @@ class VirtualToolsEndpoint(RestEndpoint):
     async def get(self) -> web.Response:
         return await self.request_success("info", f"Available tools: {TOOLS}")
 
+    async def post(self, request: web.Request) -> web.Response:
+        """
+        Uses the specified virtual tool on all virtuals.
+
+        Args:
+            request (web.Request): The request object containing the `tool` to use.
+
+        Returns:
+            web.Response: The HTTP response object.
+        """
+
+        try:
+            data = await request.json()
+        except JSONDecodeError:
+            return await self.json_decode_error()
+
+        tool = data.get("tool")
+
+        if tool is None:
+            return await self.invalid_request(
+                'Required attribute "tool" was not provided'
+            )
+
+        if tool not in TOOLS:
+            return await self.invalid_request(f"Tool {tool} is not in {TOOLS}")
+
+        if tool == "oneshot":
+            color = parse_color(validate_color(data.get("color", "white")))
+            ramp = data.get("ramp", 0)
+            hold = data.get("hold", 0)
+            fade = data.get("fade", 0)
+            brightness = min(1, max(0, data.get("brightness", 1)))
+
+            # iterate through all virtuals and apply oneshot
+            for virtual_id in self._ledfx.virtuals:
+                virtual = self._ledfx.virtuals.get(virtual_id)
+                if virtual is not None:
+                    virtual.add_oneshot(
+                        Flash(color, ramp, hold, fade, brightness)
+                    )
+
+        effect_response = {}
+        effect_response["tool"] = tool
+
+        response = {"status": "success", "tool": tool}
+        return await self.bare_request_success(response)
+
     async def put(self, request: web.Request) -> web.Response:
         """Extensible tools support"""
 
