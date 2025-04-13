4d84ea10308199addeefc2d58b9bbc797a914b61
saujanyashah <89739714+saujanyashah@users.noreply.github.com>
2025-04-13 15:00:35 +0200

    Add a docker container with Pulseaudio server running in LedFx container
    Run shairport-sync or any other app with Pulseaudio support as source for LedFx.

---
+++ ledfx_docker/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eux
+echo "Cleaning up old PulseAudio files..."
+rm -rf "$HOME/.config/pulse/"* || echo "Failed to clean Pulseaudio files. Check permissions on mounted folder."
+echo "Starting pulseaudio"
+pulseaudio --daemonize=yes --disallow-module-loading --log-target=stderr --disable-shm=yes --exit-idle-time=-1 --load="module-native-protocol-unix auth-anonymous=1 auth-cookie-enabled=0 socket=$HOME/.config/pulse/pulseaudio.socket" --load="module-always-sink"
+sleep 1
+echo "Starting ledfx"
+exec ledfx -c "$HOME/ledfx-config" "$@"
