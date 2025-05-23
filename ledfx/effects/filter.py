e989607380c43a9d0b92a929a2ad5f9ec943028a
atod <anthony.tod@gmail.com>
2025-05-23 18:15:16 -0400

    New simple filter effect

---
+++ ledfx/effects/filter.py
@@ -0,0 +1,61 @@
+import logging
+
+import numpy as np
+import voluptuous as vol
+
+from ledfx.color import parse_color, validate_color
+from ledfx.effects.audio import AudioReactiveEffect
+from ledfx.effects.gradient import GradientEffect
+
+_LOGGER = logging.getLogger(__name__)
+
+class Filter(AudioReactiveEffect, GradientEffect):
+    NAME = "Filter"
+    CATEGORY = "Simple"
+    HIDDEN_KEYS = ["background_color", "background_brightness", "blur", "mirror", "flip"]
+    ADVANCED_KEYS = []
+
+    CONFIG_SCHEMA = vol.Schema(
+        {
+            vol.Optional(
+                "color",
+                description="Simple color selector",
+                default="#FF0000",
+            ): validate_color,
+            vol.Optional(
+                "frequency_range",
+                description="Frequency range for derived brightness",
+                default="Lows (beat+bass)",
+            ): vol.In(list(AudioReactiveEffect.POWER_FUNCS_MAPPING.keys())),
+            vol.Optional(
+                "use_gradient",
+                description="Use gradient instead of color",
+                default=False,
+            ): bool,
+        }
+    )
+
+    def on_activate(self, pixel_count):
+        self.filtered_power = 0
+
+    def config_updated(self, config):
+        self.float_range = self._config["float_range"]
+        self.power_func = self.POWER_FUNCS_MAPPING[self._config["frequency_range"]]
+        self.color = np.array(parse_color(self._config["color"]))
+        self.use_gradient = self._config["use_gradient"]
+
+    def audio_data_updated(self, data):
+        self.filtered_power = getattr(data, self.power_func)()
+
+    def render(self):
+        
+        if self.use_gradient:
+            color = self.get_gradient_color(0)
+            self.roll_gradient()
+        else:
+            color = self.color
+        
+        # just fill the pixels to the selected color multiplied by the brightness
+        # we don't care if it is a single pixel or a massive matrix!
+        self.pixels[:] = color * self.filtered_power
+
