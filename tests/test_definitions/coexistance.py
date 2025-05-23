ab5407b564be598808a03c8a41ef0c43a25e2dad
atod <anthony.tod@gmail.com>
2025-05-22 23:25:47 -0400

    add more tests

--- tests/test_definitions/coexistance.py
+++ tests/test_definitions/coexistance.py
@@ -11,14 +11,8 @@ coexistance_tests = {
         payload_to_send={
             "type": "e131",
             "config": {
-                "icon_name": "mdi:led-strip",
-                "center_offset": 0,
-                "refresh_rate": 62,
                 "pixel_count": 1,
                 "universe": 50,
-                "universe_size": 510,
-                "channel_offset": 0,
-                "packet_priority": 100,
                 "name": "test_e131_1",
                 "ip_address": "1.2.3.4",
             },
@@ -43,18 +37,8 @@ coexistance_tests = {
         payload_to_send={
             "type": "artnet",
             "config": {
-                "icon_name": "mdi:led-strip",
-                "center_offset": 0,
-                "refresh_rate": 62,
                 "pixel_count": 10,
                 "universe": 10,
-                "packet_size": 510,
-                "pre_amble": "",
-                "post_amble": "",
-                "pixels_per_device": 0,
-                "dmx_start_address": 1,
-                "even_packet_size": True,
-                "output_mode": "RGB",
                 "port": 6454,
                 "ip_address": "1.2.3.4",
                 "name": "test artnet_1",
@@ -89,18 +73,8 @@ coexistance_tests = {
         payload_to_send={
             "type": "artnet",
             "config": {
-                "icon_name": "mdi:led-strip",
-                "center_offset": 0,
-                "refresh_rate": 62,
                 "pixel_count": 10,
                 "universe": 50,
-                "packet_size": 510,
-                "pre_amble": "",
-                "post_amble": "",
-                "pixels_per_device": 0,
-                "dmx_start_address": 1,
-                "even_packet_size": True,
-                "output_mode": "RGB",
                 "port": 6454,
                 "ip_address": "1.2.3.4",
                 "name": "test artnet_2",
@@ -142,9 +116,6 @@ coexistance_tests = {
         payload_to_send={
             "type": "openrgb",
             "config": {
-                "icon_name": "mdi:led-strip",
-                "center_offset": 0,
-                "refresh_rate": 62,
                 "openrgb_id": 1,
                 "pixel_count": 1,
                 "port": 6742,
@@ -172,9 +143,6 @@ coexistance_tests = {
         payload_to_send={
             "type": "openrgb",
             "config": {
-                "icon_name": "mdi:led-strip",
-                "center_offset": 0,
-                "refresh_rate": 62,
                 "openrgb_id": 2,
                 "pixel_count": 1,
                 "port": 6742,
@@ -202,13 +170,10 @@ coexistance_tests = {
         payload_to_send={
             "type": "openrgb",
             "config": {
-                "icon_name": "mdi:led-strip",
-                "center_offset": 0,
-                "refresh_rate": 62,
                 "openrgb_id": 1,
                 "pixel_count": 1,
                 "port": 6742,
-                "name": "openrgb_2",
+                "name": "openrgb_3",
                 "ip_address": "1.2.3.4",
             },
         },
@@ -240,8 +205,105 @@ coexistance_tests = {
         expected_response_values=[],
         payload_to_send={},
     ),
-    # add test cases for OSC path seperated
-    # add test cases for general port seperated
+    "cleanup_openrgb_3": APITestCase(
+        execution_order=(test_count := test_count + 1),
+        method="DELETE",
+        api_endpoint="/api/devices/test-openrgb-3",
+        expected_return_code=200,
+        expected_response_keys=[],
+        expected_response_values=[],
+        payload_to_send={},
+    ),
+    "create_osc_device_1": APITestCase(
+        execution_order=(test_count := test_count + 1),
+        method="POST",
+        api_endpoint="/api/devices",
+        expected_return_code=200,
+        payload_to_send={
+            "type": "osc",
+            "config": {
+                "port": 9000,
+                "pixel_count": 1,
+                "starting_addr": 0,
+                "path": "/0/dmx/light1",
+                "ip_address": "1.2.3.4",
+                "name": "test_osc_1"
+            }
+        },        
+        expected_response_keys=["status", "payload", "device"],
+        expected_response_values=[
+            {"status": "success"},
+            {
+                "payload": {
+                    "type": "success",
+                    "reason": "Created device test_osc_1",
+                },
+            },
+            # we are just happy it was created, we don't care about the device details
+        ],
+    ),
+    "create_osc_device_2_path_good": APITestCase(
+        execution_order=(test_count := test_count + 1),
+        method="POST",
+        api_endpoint="/api/devices",
+        expected_return_code=200,
+        payload_to_send={
+            "type": "osc",
+            "config": {
+                "port": 9000,
+                "pixel_count": 1,
+                "starting_addr": 0,
+                "path": "/0/dmx/light2",
+                "ip_address": "1.2.3.4",
+                "name": "test_osc_2"
+            }
+        },        
+        expected_response_keys=["status", "payload", "device"],
+        expected_response_values=[
+            {"status": "success"},
+            {
+                "payload": {
+                    "type": "success",
+                    "reason": "Created device test_osc_2",
+                },
+            },
+            # we are just happy it was created, we don't care about the device details
+        ],
+    ),
+    "create_osc_device_3_path_bad": APITestCase(
+        execution_order=(test_count := test_count + 1),
+        method="POST",
+        api_endpoint="/api/devices",
+        expected_return_code=200,
+        payload_to_send={
+            "type": "osc",
+            "config": {
+                "port": 9000,
+                "pixel_count": 1,
+                "starting_addr": 0,
+                "path": "/0/dmx/light1",
+                "name": "test_osc3",
+                "ip_address": "1.2.3.4"
+            }
+        },  
+        expected_response_keys=["status", "payload"],
+        expected_response_values=[
+            {"status": "failed"},
+            {
+                "payload": {
+                    "type": "error",
+                },
+            },
+        ],
+    ),
+    
+    #TODO: add test cases for general port seperated
+    #TODO: add test cases for general port seperated
+    #TODO: add test cases for general port seperated
+    #TODO: add test cases for general port seperated
+    #TODO: add test cases for general port seperated
+    #TODO: add test cases for general port seperated
+
     "cleanup_e131": APITestCase(
         execution_order=(test_count := test_count + 1),
         method="DELETE",
