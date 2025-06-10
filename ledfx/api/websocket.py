0cd2094266d1b6324d95057d4e8eee8954808009
atod <anthony.tod@gmail.com>
2025-06-09 22:03:43 -0400

    flatten to event_type

--- ledfx/api/websocket.py
+++ ledfx/api/websocket.py
@@ -224,7 +224,7 @@ class WebsocketConnection:
         _LOGGER.info("Websocket connected.")
 
         # Send UID to the client
-        await self._socket.send_json({"type": "client_id", "id": self.uid})
+        await self._socket.send_json({"event_type": "client_id", "id": self.uid})
 
         self._receiver_task = asyncio.current_task(loop=self._ledfx.loop)
         self._sender_task = self._ledfx.loop.create_task(self._sender())
