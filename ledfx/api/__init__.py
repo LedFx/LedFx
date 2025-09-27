eb39e077d461947df9ec73be0a225a5d3338b315
atod <anthony.tod@gmail.com>
2025-09-26 22:44:53 -0400

    hunt down the ci error

--- ledfx/api/__init__.py
+++ ledfx/api/__init__.py
@@ -59,7 +59,7 @@ class RestEndpoint(BaseRegistry):
                 }
             )
         except Exception as e:
-            # _LOGGER.exception(e)
+            _LOGGER.exception(f"API Exception in {request.method} {request.path}: {e}")
             reason = getattr(e, "args", None)
             if reason:
                 reason = str(reason[0])
